048a0c638fe24754049bb13fd5fa775b05532ae3
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-11-08 09:00:44 +0000

    Parameters will be empty for the very first build, setting an environment variable

--- .ci/pipelines/acceptance.groovy
+++ .ci/pipelines/acceptance.groovy
@@ -7,6 +7,10 @@ pipeline {
     REPOSITORY = "terraform-provider-elasticstack"
     GIT_REFERENCE_REPO = "/var/lib/jenkins/.git-references/terraform-provider-elasticstack.git"
     GIT_CREDENTIALS = "f6c7695a-671e-4f4f-a331-acdce44ff9ba"
+    branch_specifier = "${params?.branch_specifier}"
+  }
+  parameters {
+    string(name: 'branch_specifier', defaultValue: 'refs/heads/main', description: "the Git branch specifier to build (&lt;branchName&gt;, &lt;tagName&gt;,&lt;commitId&gt;, etc.)")
   }
   stages {
     stage('Checkout') {
@@ -14,7 +18,7 @@ pipeline {
       steps {
         estcGithubCheckout(github_org: 'elastic',
                             repository: env.REPOSITORY,
-                            revision: env.ghprbActualCommit ?: env.CHANGE_BRANCH ?: env.BRANCH_NAME ?: params.branch_specifier,
+                            revision: env.ghprbActualCommit ?: env.CHANGE_BRANCH ?: env.BRANCH_NAME ?: env.branch_specifier,
                             reference_repo: env.GIT_REFERENCE_REPO,
                             credentials: env.GIT_CREDENTIALS)
       }
