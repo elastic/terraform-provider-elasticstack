ed6285eb194d50e93c3139bd73f039fca2fd083e
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-11-08 09:18:01 +0000

    Use default credentials

--- .ci/pipelines/acceptance.groovy
+++ .ci/pipelines/acceptance.groovy
@@ -6,7 +6,6 @@ pipeline {
     HOME = "${env.JENKINS_HOME}"
     REPOSITORY = "terraform-provider-elasticstack"
     GIT_REFERENCE_REPO = "/var/lib/jenkins/.git-references/terraform-provider-elasticstack.git"
-    GIT_CREDENTIALS = "f6c7695a-671e-4f4f-a331-acdce44ff9ba"
     branch_specifier = "${params?.branch_specifier}"
   }
   parameters {
@@ -19,8 +18,7 @@ pipeline {
         estcGithubCheckout(github_org: 'elastic',
                             repository: env.REPOSITORY,
                             revision: env.ghprbActualCommit ?: env.CHANGE_BRANCH ?: env.BRANCH_NAME ?: env.branch_specifier,
-                            reference_repo: env.GIT_REFERENCE_REPO,
-                            credentials: env.GIT_CREDENTIALS)
+                            reference_repo: env.GIT_REFERENCE_REPO)
       }
     }
     stage('Acceptance Tests') {
