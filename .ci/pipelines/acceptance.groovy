ebd771636553c69122f8939e3f0afdf967b6ad2b
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-11-08 08:58:52 +0000

    CI: minor changes

--- .ci/pipelines/acceptance.groovy
+++ .ci/pipelines/acceptance.groovy
@@ -2,14 +2,12 @@
 
 pipeline {
   agent { label('linux && immutable && docker') }
-
   environment {
-    HOME = "${env.JENKINS_HOME}" 
+    HOME = "${env.JENKINS_HOME}"
     REPOSITORY = "terraform-provider-elasticstack"
     GIT_REFERENCE_REPO = "/var/lib/jenkins/.git-references/terraform-provider-elasticstack.git"
     GIT_CREDENTIALS = "f6c7695a-671e-4f4f-a331-acdce44ff9ba"
   }
-  
   stages {
     stage('Checkout') {
       options { skipDefaultCheckout() }
@@ -19,20 +17,12 @@ pipeline {
                             revision: env.ghprbActualCommit ?: env.CHANGE_BRANCH ?: env.BRANCH_NAME ?: params.branch_specifier,
                             reference_repo: env.GIT_REFERENCE_REPO,
                             credentials: env.GIT_CREDENTIALS)
-      } 
+      }
     }
-    
     stage('Acceptance Tests') {
       steps {
-        script {
-          dir('.') {
-            sh '''#!/bin/bash
-
-              make docker-testacc
-            '''
-          }
-        }
-      } 
+        sh(label: 'Run ATs in docker', script: 'make docker-testacc')
+      }
     }
   }
 }
