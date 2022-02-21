5372001c91eafd6a0787221adb14a558c20102de
Oleksandr Kylymnychenko <oleksandr.kylymnychenko@elastic.co>
2022-02-21 15:28:06 +0100

    Convert CI job to matrix one
    Will be used to test different versions of ES with this provider

--- .ci/pipelines/acceptance.groovy
+++ .ci/pipelines/acceptance.groovy
@@ -1,30 +1,43 @@
 @Library('estc') _
 
 pipeline {
-  agent { label('linux && immutable && docker') }
-  environment {
-    HOME = "${env.JENKINS_HOME}"
-    REPOSITORY = "terraform-provider-elasticstack"
-    GIT_REFERENCE_REPO = "/var/lib/jenkins/.git-references/terraform-provider-elasticstack.git"
-    branch_specifier = "${params?.branch_specifier}"
-  }
   parameters {
     string(name: 'branch_specifier', defaultValue: 'refs/heads/main', description: "the Git branch specifier to build (&lt;branchName&gt;, &lt;tagName&gt;,&lt;commitId&gt;, etc.)")
   }
+  agent none
   stages {
-    stage('Checkout') {
-      options { skipDefaultCheckout() }
-      steps {
-        estcGithubCheckout(github_org: 'elastic',
-                            repository: env.REPOSITORY,
-                            revision: env.ghprbActualCommit ?: env.CHANGE_BRANCH ?: env.BRANCH_NAME ?: env.branch_specifier,
-                            reference_repo: env.GIT_REFERENCE_REPO)
-      }
-    }
     stage('Acceptance Tests') {
-      steps {
-        dir('.') {
-          sh(label: 'Run ATs in docker', script: 'make docker-testacc')
+      matrix {
+        axes {
+          axis {
+            name 'ES_VERSION'
+            values '8.0.0', '7.15.1'
+          }
+        }
+        agent { label('linux && immutable && docker') }
+        environment {
+          HOME = "${env.JENKINS_HOME}"
+          REPOSITORY = "terraform-provider-elasticstack"
+          GIT_REFERENCE_REPO = "/var/lib/jenkins/.git-references/terraform-provider-elasticstack.git"
+          branch_specifier = "${params?.branch_specifier}"
+        }
+        stages {
+          stage('Checkout') {
+            options { skipDefaultCheckout() }
+            steps {
+              estcGithubCheckout(github_org: 'elastic',
+                                  repository: env.REPOSITORY,
+                                  revision: env.ghprbActualCommit ?: env.CHANGE_BRANCH ?: env.BRANCH_NAME ?: env.branch_specifier,
+                                  reference_repo: env.GIT_REFERENCE_REPO)
+            }
+          }
+          stage('Tests') {
+            steps {
+              dir('.') {
+                sh(label: 'Run ATs in docker', script: "ELASTICSEARCH_VERSION=${ES_VERSION} make docker-testacc")
+              }
+            }
+          }
         }
       }
     }
