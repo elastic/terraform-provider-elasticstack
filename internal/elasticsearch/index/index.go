67cc00a0fa9a21a6be197dbcf464af4ff8b08d10
k-yomo <kanji.yy@gmail.com>
2022-07-05 23:28:17 +0900

    Fix invalid links in the document for index resource

--- internal/elasticsearch/index/index.go
+++ internal/elasticsearch/index/index.go
@@ -91,7 +91,7 @@ func ResourceIndex() *schema.Resource {
 		},
 		"mappings": {
 			Description: `Mapping for fields in the index.
-If specified, this mapping can include: field names, field data types (https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-types.html), mapping parameters (https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-params.html).
+If specified, this mapping can include: field names, [field data types](https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-types.html), [mapping parameters](https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-params.html).
 **NOTE:** changing datatypes in the existing _mappings_ will force index to be re-created.`,
 			Type:             schema.TypeString,
 			Optional:         true,
