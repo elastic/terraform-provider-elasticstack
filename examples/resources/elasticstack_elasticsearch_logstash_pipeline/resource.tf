ca81292d485efd60571b272970616a77eb8a71e0
Robson Sutton <robson.sutton@nationwide.co.uk>
2022-10-14 17:57:31 +0100

    reformat example

--- examples/resources/elasticstack_elasticsearch_logstash_pipeline/resource.tf
+++ examples/resources/elasticstack_elasticsearch_logstash_pipeline/resource.tf
@@ -12,20 +12,20 @@ resource "elasticstack_elasticsearch_logstash_pipeline" "example" {
   output{}
 EOF
 
-  pipeline_metadata = jsonencode({
+  pipeline_metadata = {
     "type"    = "logstash_pipeline"
-    "version" = "1"
-  })
+    "version" = 1
+  }
 
-  pipeline_settings = jsonencode({
-    "pipeline.workers" : 1,
-    "pipeline.batch.size" : 125,
-    "pipeline.batch.delay" : 50,
-    "queue.type" : "memory",
-    "queue.max_bytes.number" : 1,
-    "queue.max_bytes.units" : "gb",
-    "queue.checkpoint.writes" : 1024
-  })
+  pipeline_settings = {
+    "pipeline.workers"        = 2
+    "pipeline.batch.size"     = 250
+    "pipeline.batch.delay"    = 100
+    "queue.type"              = "persisted"
+    "queue.max_bytes.number"  = 2
+    "queue.max_bytes.units"   = "mb"
+    "queue.checkpoint.writes" = 2048
+  }
 }
 
 output "pipeline" {
