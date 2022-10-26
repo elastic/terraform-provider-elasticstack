ebac0e23d9a02e5622390f31a174af071736310f
k-yomo <k-yomoda@mercari.com>
2022-10-26 20:42:26 +0900

    Remove ForceNew from analysis settings

--- internal/elasticsearch/index/index.go
+++ internal/elasticsearch/index/index.go
@@ -376,39 +376,36 @@ func ResourceIndex() *schema.Resource {
 			Description: "Set the number of characters of the `_source` to include in the slowlog lines, `false` or `0` will skip logging the source entirely and setting it to `true` will log the entire source regardless of size. The original `_source` is reformatted by default to make sure that it fits on a single log line.",
 			Optional:    true,
 		},
+		// To change analyzer setting, the index must be closed, updated, and then reopened but it can't be handled in terraform.
+		// We raise error when they are tried to be updated instead of setting ForceNew not to have unexpected deletion.
 		"analysis_analyzer": {
 			Type:         schema.TypeString,
 			Description:  "A JSON string describing the analyzers applied to the index.",
 			Optional:     true,
-			ForceNew:     true, // To add an analyzer, the index must be closed, updated, and then reopened; we can't handle that here.
 			ValidateFunc: validation.StringIsJSON,
 		},
 		"analysis_tokenizer": {
 			Type:         schema.TypeString,
 			Description:  "A JSON string describing the tokenizers applied to the index.",
 			Optional:     true,
-			ForceNew:     true, // To add a tokenizer, the index must be closed, updated, and then reopened; we can't handle that here.
 			ValidateFunc: validation.StringIsJSON,
 		},
 		"analysis_char_filter": {
 			Type:         schema.TypeString,
 			Description:  "A JSON string describing the char_filters applied to the index.",
 			Optional:     true,
-			ForceNew:     true, // To add a char_filters, the index must be closed, updated, and then reopened; we can't handle that here.
 			ValidateFunc: validation.StringIsJSON,
 		},
 		"analysis_filter": {
 			Type:         schema.TypeString,
 			Description:  "A JSON string describing the filters applied to the index.",
 			Optional:     true,
-			ForceNew:     true, // To add a filter, the index must be closed, updated, and then reopened; we can't handle that here.
 			ValidateFunc: validation.StringIsJSON,
 		},
 		"analysis_normalizer": {
 			Type:         schema.TypeString,
 			Description:  "A JSON string describing the normalizers applied to the index.",
 			Optional:     true,
-			ForceNew:     true, // To add a normalizer, the index must be closed, updated, and then reopened; we can't handle that here.
 			ValidateFunc: validation.StringIsJSON,
 		},
 		"alias": {
