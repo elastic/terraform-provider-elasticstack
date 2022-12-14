b81c238001db0491b0cf112d43b718f8f9093361
Adrian Bruinhout <adrian@webfella.com.au>
2022-12-14 20:35:05 +1100

    Adding Kibana connection schema

--- docs/index.md
+++ docs/index.md
@@ -86,6 +86,7 @@ provider "elasticstack" {
 ### Optional
 
 - `elasticsearch` (Block List, Max: 1) Elasticsearch connection configuration block. (see [below for nested schema](#nestedblock--elasticsearch))
+- `kibana` (Block List, Max: 1) Kibana connection configuration block. (see [below for nested schema](#nestedblock--kibana))
 
 <a id="nestedblock--elasticsearch"></a>
 ### Nested Schema for `elasticsearch`
@@ -101,5 +102,17 @@ Optional:
 - `insecure` (Boolean) Disable TLS certificate validation
 - `key_data` (String, Sensitive) PEM encoded private key for client auth
 - `key_file` (String) Path to a file containing the PEM encoded private key for client auth
+- `password` (String, Sensitive) Password to use for API authentication to Elasticsearch.
+- `username` (String) Username to use for API authentication to Elasticsearch.
+
+
+<a id="nestedblock--kibana"></a>
+### Nested Schema for `kibana`
+
+Optional:
+
+- `api_key` (String, Sensitive) API Key to use for authentication to Elasticsearch
+- `endpoints` (List of String, Sensitive) A comma-separated list of endpoints where the terraform provider will point to, this must include the http(s) schema and port number.
+- `insecure` (Boolean) Disable TLS certificate validation
 - `password` (String, Sensitive) Password to use for API authentication to Elasticsearch.
 - `username` (String) Username to use for API authentication to Elasticsearch.
--- provider/provider.go
+++ provider/provider.go
@@ -21,9 +21,59 @@ func init() {
 
 func New(version string) *schema.Provider {
 	p := &schema.Provider{
-
 		Schema: map[string]*schema.Schema{
 			esKeyName: providerSchema.GetConnectionSchema(esKeyName, true),
+			"kibana": {
+				Description: "Kibana connection configuration block.",
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Optional:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"username": {
+							Description:  "Username to use for API authentication to Elasticsearch.",
+							Type:         schema.TypeString,
+							Optional:     true,
+							DefaultFunc:  schema.EnvDefaultFunc("ELASTICSEARCH_USERNAME", nil),
+							RequiredWith: []string{"kibana.0.password"},
+							ExactlyOneOf: []string{"elasticsearch.0.username", "kibana.0.username"},
+						},
+						"password": {
+							Description:  "Password to use for API authentication to Elasticsearch.",
+							Type:         schema.TypeString,
+							Optional:     true,
+							Sensitive:    true,
+							DefaultFunc:  schema.EnvDefaultFunc("ELASTICSEARCH_PASSWORD", nil),
+							RequiredWith: []string{"kibana.0.username"},
+							ExactlyOneOf: []string{"elasticsearch.0.password", "kibana.0.password"},
+						},
+						"api_key": {
+							Description:   "API Key to use for authentication to Elasticsearch",
+							Type:          schema.TypeString,
+							Optional:      true,
+							Sensitive:     true,
+							DefaultFunc:   schema.EnvDefaultFunc("ELASTICSEARCH_API_KEY", nil),
+							ConflictsWith: []string{"kibana.0.username", "kibana.0.password"},
+							ExactlyOneOf:  []string{"elasticsearch.0.api_key", "kibana.0.api_key"},
+						},
+						"endpoints": {
+							Description: "A comma-separated list of endpoints where the terraform provider will point to, this must include the http(s) schema and port number.",
+							Type:        schema.TypeList,
+							Optional:    true,
+							Sensitive:   true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"insecure": {
+							Description: "Disable TLS certificate validation",
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Default:     false,
+						},
+					},
+				},
+			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"elasticstack_elasticsearch_ingest_processor_append":            ingest.DataSourceProcessorAppend(),
