d71a13dbedb3f29cd1e7af3a9537c4e1eca94cd3
k-yomo <kanji.yy@gmail.com>
2022-12-18 19:35:59 +0900

    Add framework provider

--- provider/factory.go
+++ provider/factory.go
@@ -2,6 +2,9 @@ package provider
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
@@ -10,14 +13,16 @@ import (
 // ProtoV5ProviderServerFactory returns a muxed terraform-plugin-go protocol v5 provider factory function.
 func ProtoV5ProviderServerFactory(ctx context.Context, version string) (func() tfprotov5.ProviderServer, error) {
 	sdkv2Provider := New(version)
+	fwProvider := providerserver.NewProtocol5(NewFrameworkProvider(version))
 
 	servers := []func() tfprotov5.ProviderServer{
+		fwProvider,
 		sdkv2Provider.GRPCProvider,
 	}
 
 	muxServer, err := tf5muxserver.NewMuxServer(ctx, servers...)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("initialize mux server: %w", err)
 	}
 
 	return muxServer.ProviderServer, nil
---
+++ provider/fwprovider.go
@@ -0,0 +1,61 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/elastic/terraform-provider-elasticstack/internal/clients"
+	"github.com/elastic/terraform-provider-elasticstack/internal/schema"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	fwprovider "github.com/hashicorp/terraform-plugin-framework/provider"
+	fwschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+type Provider struct {
+	version string
+}
+
+// NewFrameworkProvider instantiates plugin framework's provider
+func NewFrameworkProvider(version string) fwprovider.Provider {
+	return &Provider{
+		version: version,
+	}
+}
+
+func (p *Provider) Metadata(_ context.Context, _ fwprovider.MetadataRequest, res *fwprovider.MetadataResponse) {
+	res.TypeName = "elasticstack"
+	res.Version = p.version
+}
+
+func (p *Provider) Schema(ctx context.Context, req fwprovider.SchemaRequest, res *fwprovider.SchemaResponse) {
+	res.Schema = fwschema.Schema{
+		Blocks: map[string]fwschema.Block{
+			esKeyName: schema.GetFWConnectionBlock(esKeyName, true),
+		},
+	}
+}
+
+func (p *Provider) Configure(ctx context.Context, req fwprovider.ConfigureRequest, res *fwprovider.ConfigureResponse) {
+	esConn := []*clients.ElasticSearchConnection{}
+	diags := req.Config.GetAttribute(ctx, path.Root(esKeyName), &esConn)
+	res.Diagnostics.Append(diags...)
+	if res.Diagnostics.HasError() {
+		return
+	}
+	apiClient, diags := clients.NewFWEsApiClient(ctx, esConn[0], p.version, true)
+	res.Diagnostics.Append(diags...)
+	if res.Diagnostics.HasError() {
+		return
+	}
+	res.DataSourceData = apiClient
+	res.ResourceData = apiClient
+}
+
+func (p *Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
+}
+
+func (p *Provider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
+}
