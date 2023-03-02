ec2c0dc64afe8f6065e7aa581a55a83e7303a750
Robson Sutton <robson.sutton@nationwide.co.uk>
2023-03-02 13:24:50 +0000

    testing

--- internal/clients/elasticsearch/watch.go
+++ internal/clients/elasticsearch/watch.go
@@ -70,14 +70,14 @@ func GetWatch(ctx context.Context, apiClient *clients.ApiClient, watchID string)
 	}
 
 	watch := make(map[string]models.Watch)
-	watchBody := make(map[string]interface{})
-	if err := json.NewDecoder(res.Body).Decode(&watchBody); err != nil {
-		return nil, diag.FromErr(err)
-	}
+	// watchBody := make(map[string]interface{})
+	// if err := json.NewDecoder(res.Body).Decode(&watchBody); err != nil {
+	// 	return nil, diag.FromErr(err)
+	// }
 
 	if watch, ok := watch[watchID]; ok {
 		watch.WatchID = watchID
-		//watch.Body = watchBody
+		// watch.Body = watchBody
 		return &watch, diags
 	}
 
