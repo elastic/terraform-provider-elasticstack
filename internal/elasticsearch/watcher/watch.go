c06059d603569ca5fe84ed6e54b9e161472c5bb8
Robson Sutton <robson.sutton@nationwide.co.uk>
2023-03-06 18:01:49 +0000

    testing

--- internal/elasticsearch/watcher/watch.go
+++ internal/elasticsearch/watcher/watch.go
@@ -77,7 +77,7 @@ func resourceWatchPut(ctx context.Context, d *schema.ResourceData, meta interfac
 	var watchBody models.WatchBody
 
 	if watchBody.Actions == nil {
-		v := map[string]interface{}{}
+		v := make(map[string]interface{})
 		watchBody.Actions = &v
 	}
 	if watchBody.Condition == nil {
