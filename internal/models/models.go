0cdebd3ca3ef11a2a3581ef6b96127b69f94841b
Robson Sutton <robson.sutton@nationwide.co.uk>
2023-03-13 09:04:54 +0000

    testing

--- internal/models/models.go
+++ internal/models/models.go
@@ -276,11 +276,11 @@ type Watch struct {
 }
 
 type WatchBody struct {
-	Actions                   *map[string]interface{} `json:"actions"`
-	Condition                 *map[string]interface{} `json:"condition"`
-	Input                     *map[string]interface{} `json:"input"`
-	Trigger                   *map[string]interface{} `json:"trigger"`
-	Metadata                  *map[string]interface{} `json:"metadata"`
-	Throttle_period           *string                 `json:"throttle_period"`
-	Throttle_period_in_millis *int                    `json:"throttle_period_in_millis"`
+	Actions                   *map[string]interface{} `json:"actions,omitempty"`
+	Condition                 *map[string]interface{} `json:"condition,omitempty"`
+	Input                     *map[string]interface{} `json:"input,omitempty"`
+	Trigger                   *map[string]interface{} `json:"trigger,omitempty"`
+	Metadata                  *map[string]interface{} `json:"metadata,omitempty"`
+	Throttle_period           *string                 `json:"throttle_period,omitempty"`
+	Throttle_period_in_millis *int                    `json:"throttle_period_in_millis,omitempty"`
 }
