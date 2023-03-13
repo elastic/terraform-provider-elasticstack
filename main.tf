849e10f6b6a2ea53c6c2ebc5fd76be6fa97480be
Robson Sutton <robson.sutton@nationwide.co.uk>
2023-03-13 18:05:02 +0000

    validate omitempty on metadata field

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    elasticstack = {
+      source  = "elastic/elasticstack"
+      version = "0.5.0"
+    }
+  }
+}
+
+provider "elasticstack" {
+  elasticsearch {}
+}
+
+resource "elasticstack_elasticsearch_watcher_watch" "example" {
+  watch_id = "test"
+  active   = true
+
+  body = jsonencode({
+    "trigger" = {
+      "schedule" = { "cron" = "0 0/1 * * * ?" }
+    },
+    "input" = {
+      "none" = {}
+    },
+    "condition" = {
+      "always" = {}
+    },
+    "actions" = {}
+    # "metadata" = {},
+    "throttle_period_in_millis" = 5000
+  })
+}
+
+# output "watch" {
+#   value = elasticstack_elasticsearch_watcher_watch.example.watch_id
+# }
