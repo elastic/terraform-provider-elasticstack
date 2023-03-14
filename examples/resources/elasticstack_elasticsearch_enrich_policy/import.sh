94d742a75036876cc168d0b7419846d7ebdcad6f
jonjitsu <elastic@jonathanwillis.ca>
2023-03-14 15:03:21 -0400

    enrich/policy: add import.sh to example

---
+++ examples/resources/elasticstack_elasticsearch_enrich_policy/import.sh
@@ -0,0 +1,4 @@
+# NOTE: while importing index resource, keep in mind, that some of the default index settings will be imported into the TF state too
+# You can later adjust the index configuration to account for those imported settings
+terraform import elasticstack_elasticsearch_index.my_index <cluster_uuid>/<index_name>
+terraform import elasticstack_elasticsearch_enrich_policy.policy1 <cluster_uuid>/<policy_name>
