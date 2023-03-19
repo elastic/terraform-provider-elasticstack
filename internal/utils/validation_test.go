b134508ab3887bff32bc827d5132dbb1f6d07ead
Cristian Georgiu <64313606+Cristigeo@users.noreply.github.com>
2023-03-20 01:05:33 +0200

    Support for transforms (#284)
    * initial commit
    
    * support for Update
    
    * adding missing properties for transform
    
    * misc corrections
    
    * some documentation
    
    * support for transform start/stop
    
    * addiong options on the client calls for transform start/stop
    
    * initial commit
    
    * support for Update
    
    * adding missing properties for transform
    
    * misc corrections
    
    * some documentation
    
    * support for transform start/stop
    
    * addiong options on the client calls for transform start/stop
    
    * code updates based on feedback
    
    * updated documentation (added md template)
    
    * timeout query param is only available from v7.17.0
    
    * fixed the messed up md file
    
    * settings promoted to individual arguments; drift detection for updatable props
    
    * check versions before using features
    
    * updated doc
    
    * support for terraform import
    
    * minor corrections/updates
    
    * corrected min version for align_checkpoints
    
    * updates on acc tests
    
    * removed some defaults; updated docs; updated acc test

--- internal/utils/validation_test.go
+++ internal/utils/validation_test.go
@@ -46,3 +46,50 @@ func TestStringIsDuration(t *testing.T) {
 		})
 	}
 }
+
+func TestStringIsElasticDuration(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		i            interface{}
+		k            string
+		wantWarnings []string
+		wantErrors   []error
+	}{
+		{
+			name: "valid Elastic duration string",
+			i:    "30d",
+			k:    "delay",
+		},
+		{
+			name:       "invalid Elastic duration unit",
+			i:          "12w",
+			k:          "delay",
+			wantErrors: []error{errors.New(`"delay" contains an invalid duration: not conforming to Elastic time-units format`)},
+		},
+		{
+			name:       "invalid Elastic duration value",
+			i:          ".12s",
+			k:          "delay",
+			wantErrors: []error{errors.New(`"delay" contains an invalid duration: not conforming to Elastic time-units format`)},
+		},
+		{
+			name:       "invalid data type",
+			i:          30,
+			k:          "delay",
+			wantErrors: []error{errors.New("expected type of delay to be string")},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotWarnings, gotErrors := StringIsElasticDuration(tt.i, tt.k)
+			if !reflect.DeepEqual(gotWarnings, tt.wantWarnings) {
+				t.Errorf("StringIsElasticDuration() gotWarnings = %v, want %v", gotWarnings, tt.wantWarnings)
+			}
+			if !reflect.DeepEqual(gotErrors, tt.wantErrors) {
+				t.Errorf("StringIsElasticDuration() gotErrors = %v, want %v", gotErrors, tt.wantErrors)
+			}
+		})
+	}
+}
