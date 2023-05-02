6e3cffdb038bade041d5fd24b7b4e846ee483a16
Taylor Swanson <taylor.swanson@elastic.co>
2023-05-02 10:47:13 -0500

    Revert debug.go back to what's in main

--- internal/clients/debug.go
+++ internal/clients/debug.go
@@ -30,57 +30,57 @@ type debugLogger struct {
 	Name string
 }
 
-func (l *debugLogger) LogRoundTrip(req *http.Request, resp *http.Response, err error, _ time.Time, duration time.Duration) error {
+func (l *debugLogger) LogRoundTrip(req *http.Request, resp *http.Response, err error, start time.Time, duration time.Duration) error {
 	ctx := req.Context()
-	requestID := "<nil>"
+	requestId := "<nil>"
 	if req != nil {
-		requestID = fmt.Sprintf("%s %s", req.Method, req.URL)
+		requestId = fmt.Sprintf("%s %s", req.Method, req.URL)
 	}
-	tflog.Debug(ctx, fmt.Sprintf("%s request [%s] executed. Took %s. %#v", l.Name, requestID, duration, err))
+	tflog.Debug(ctx, fmt.Sprintf("%s request [%s] executed. Took %s. %#v", l.Name, requestId, duration, err))
 
 	if req != nil && req.Body != nil {
-		l.logRequest(ctx, req, requestID)
+		l.logRequest(ctx, req, requestId)
 	}
 
 	if resp != nil && resp.Body != nil {
-		l.logResponse(ctx, resp, requestID)
+		l.logResponse(ctx, resp, requestId)
 	}
 
 	if resp == nil {
-		tflog.Debug(ctx, fmt.Sprintf("%s response for [%s] is nil", l.Name, requestID))
+		tflog.Debug(ctx, fmt.Sprintf("%s response for [%s] is nil", l.Name, requestId))
 	}
 
 	return nil
 }
 
-func (l *debugLogger) logRequest(ctx context.Context, req *http.Request, _ string) {
+func (l *debugLogger) logRequest(ctx context.Context, req *http.Request, requestId string) {
 	defer req.Body.Close()
 
 	reqData, err := httputil.DumpRequestOut(req, true)
 	if err == nil {
-		tflog.Debug(ctx, fmt.Sprintf(logReqMsg, l.Name, prettyPrintJSONLines(reqData)))
+		tflog.Debug(ctx, fmt.Sprintf(logReqMsg, l.Name, prettyPrintJsonLines(reqData)))
 	} else {
 		tflog.Debug(ctx, fmt.Sprintf("%s API request dump error: %#v", l.Name, err))
 	}
 }
 
-func (l *debugLogger) logResponse(ctx context.Context, resp *http.Response, requestID string) {
+func (l *debugLogger) logResponse(ctx context.Context, resp *http.Response, requestId string) {
 	defer resp.Body.Close()
 
 	respData, err := httputil.DumpResponse(resp, true)
 	if err == nil {
-		tflog.Debug(ctx, fmt.Sprintf(logRespMsg, l.Name, requestID, prettyPrintJSONLines(respData)))
+		tflog.Debug(ctx, fmt.Sprintf(logRespMsg, l.Name, requestId, prettyPrintJsonLines(respData)))
 	} else {
-		tflog.Debug(ctx, fmt.Sprintf("%s API response for [%s] dump error: %#v", l.Name, requestID, err))
+		tflog.Debug(ctx, fmt.Sprintf("%s API response for [%s] dump error: %#v", l.Name, requestId, err))
 	}
 }
 
-func (*debugLogger) RequestBodyEnabled() bool  { return true }
-func (*debugLogger) ResponseBodyEnabled() bool { return true }
+func (l *debugLogger) RequestBodyEnabled() bool  { return true }
+func (l *debugLogger) ResponseBodyEnabled() bool { return true }
 
-// prettyPrintJSONLines iterates through a []byte line-by-line,
+// prettyPrintJsonLines iterates through a []byte line-by-line,
 // transforming any lines that are complete json into pretty-printed json.
-func prettyPrintJSONLines(b []byte) string {
+func prettyPrintJsonLines(b []byte) string {
 	parts := strings.Split(string(b), "\n")
 	for i, p := range parts {
 		if b := []byte(p); json.Valid(b) {
