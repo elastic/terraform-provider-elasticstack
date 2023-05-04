4252ddc6d9217704e69aeca6fcae9ed679d04d5c
Taylor Swanson <taylor.swanson@elastic.co>
2023-05-04 10:39:23 -0500

    Clean up generate.go

--- internal/clients/fleet/fleetapi/generate.go
+++ internal/clients/fleet/fleetapi/generate.go
@@ -5,28 +5,42 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 const (
-	fleetOpenapiURL = "https://raw.githubusercontent.com/elastic/kibana/main/x-pack/plugins/fleet/common/openapi/bundled.json"
+	fleetSchemaURLTmpl = "https://raw.githubusercontent.com/elastic/kibana/%s/x-pack/plugins/fleet/common/openapi/bundled.json"
 )
 
-type OpenAPI struct {
-	Other map[string]any            `yaml:",inline"`
-	Paths map[string]map[string]any `yaml:"paths"`
+type OpenAPISchema struct {
+	Paths          map[string]*Path `json:"paths"`
+	OpenAPIVersion string           `json:"openapi"`
+	Tags           []any            `json:"tags,omitempty"`
+	Servers        []any            `json:"servers,omitempty"`
+	Components     map[string]any   `json:"components,omitempty"`
+	Security       []any            `json:"security,omitempty"`
 }
 
 type Path struct {
-	Other      map[string]any   `yaml:",inline"`
-	Parameters []map[string]any `yaml:"parameters,omitempty"`
+	Parameters []map[string]any `json:"parameters,omitempty"`
+	Get        *Endpoint        `json:"get,omitempty"`
+	Post       *Endpoint        `json:"post,omitempty"`
+	Put        *Endpoint        `json:"put,omitempty"`
+	Delete     *Endpoint        `json:"delete,omitempty"`
+}
+
+type Endpoint struct {
+	Summary     string           `json:"summary,omitempty"`
+	Tags        []string         `json:"tags,omitempty"`
+	Responses   map[string]any   `json:"responses,omitempty"`
+	OperationID string           `json:"operationId,omitempty"`
+	Parameters  []map[string]any `json:"parameters,omitempty"`
+	Deprecated  bool             `json:"deprecated,omitempty"`
 }
 
 var includePaths = map[string][]string{}
@@ -45,9 +59,41 @@ func downloadFile(url string) ([]byte, error) {
 	return io.ReadAll(resp.Body)
 }
 
+func stringInSlice(value string, slice []string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+// filterKbnXsrfParameter filters out an entry if it is a kbn_xsrf parameter.
+// Returns a copy of the slice if it was modified, otherwise returns the original
+// slice if no match was found.
+func filterKbnXsrfParameter(parameters []map[string]any) []map[string]any {
+	removeIndex := -1
+
+	for i, param := range parameters {
+		if ref, ok := param["$ref"].(string); ok && ref == "#/components/parameters/kbn_xsrf" {
+			removeIndex = i
+			break
+		}
+	}
+	if removeIndex != -1 {
+		ret := make([]map[string]any, 0)
+		ret = append(ret, parameters[:removeIndex]...)
+		return append(ret, parameters[removeIndex+1:]...)
+	}
+
+	return parameters
+}
+
 func main() {
 	outFile := flag.String("o", "", "output file")
 	inFile := flag.String("i", "", "input file")
+	apiVersion := flag.String("v", "main", "api version")
 	flag.Parse()
 
 	if *outFile == "" {
@@ -56,257 +102,58 @@ func main() {
 	}
 
 	var err error
-	var openapiYAML []byte
+	var schemaData []byte
+
 	if *inFile != "" {
-		openapiYAML, err = os.ReadFile(*inFile)
+		schemaData, err = os.ReadFile(*inFile)
 	} else {
-		openapiYAML, err = downloadFile(fleetOpenapiURL)
+		schemaData, err = downloadFile(fmt.Sprintf(fleetSchemaURLTmpl, *apiVersion))
 	}
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var openapi M
-	if err := json.Unmarshal(openapiYAML, &openapi); err != nil {
+	var schema OpenAPISchema
+	if err = json.Unmarshal(schemaData, &schema); err != nil {
 		log.Fatal(err)
 	}
 
-	openapiPaths := openapi["paths"].(map[string]any)
-	for path, pathData := range openapiPaths {
-		pathDataMap := pathData.(map[string]any)
-		removeParametersKbnXsrf(pathDataMap)
-
-		if allowedOps, found := includePaths[path]; found {
-		nextOp:
-			for op := range pathDataMap {
-				for _, allowedOp := range allowedOps {
-					if op == allowedOp || op == "parameters" {
-						continue nextOp
-					}
-				}
-
-				delete(pathDataMap, op)
-			}
+	for path, pathInfo := range schema.Paths {
+		// Remove paths not in filter list.
+		if _, exists := includePaths[path]; !exists {
+			delete(schema.Paths, path)
 			continue
 		}
 
-		delete(openapiPaths, path)
+		// Filter out kbn-xsrf parameter (already set by API client).
+		pathInfo.Parameters = filterKbnXsrfParameter(pathInfo.Parameters)
+
+		// Filter out endpoints not if filter list, filter out kbn-xsrf
+		// parameter in endpoint (already set by API client).
+		allowedMethods := includePaths[path]
+		filterEndpointFn := func(endpoint *Endpoint, method string) *Endpoint {
+			if endpoint == nil {
+				return nil
+			}
+			if !stringInSlice(method, allowedMethods) {
+				return nil
+			}
+
+			endpoint.Parameters = filterKbnXsrfParameter(endpoint.Parameters)
+
+			return endpoint
+		}
+		pathInfo.Get = filterEndpointFn(pathInfo.Get, "get")
+		pathInfo.Post = filterEndpointFn(pathInfo.Post, "post")
+		pathInfo.Put = filterEndpointFn(pathInfo.Put, "put")
+		pathInfo.Delete = filterEndpointFn(pathInfo.Delete, "delete")
 	}
 
-	f, err := os.Create(*outFile)
+	outData, err := json.MarshalIndent(&schema, "", "  ")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
-
-	openapi.Put("components.schemas.package_policy_request_input",
-		openapi.Get("components.schemas.package_policy_request.properties.inputs.additionalProperties"))
-	openapi.Put("components.schemas.package_policy_request_input_stream",
-		openapi.Get("components.schemas.package_policy_request.properties.inputs.additionalProperties.properties.streams.additionalProperties"))
-
-	openapi.Put("components.schemas.package_policy_request.properties.inputs.additionalProperties.properties.streams.additionalProperties", map[string]any{
-		"$ref": "#/components/schemas/package_policy_request_input_stream",
-	})
-	openapi.Put("components.schemas.package_policy_request.properties.inputs.additionalProperties", map[string]any{
-		"$ref": "#/components/schemas/package_policy_request_input",
-	})
-
-	// Add "policy_template" to input.
-	openapi.Put("components.schemas.new_package_policy.properties.inputs.items.properties.policy_template", map[string]any{
-		"type": "string",
-	})
-	openapi.Append("components.schemas.new_package_policy.properties.inputs.items.required", "policy_template")
-
-	// Add properties to "streams".
-	openapi.Put("components.schemas.new_package_policy.properties.inputs.items.properties.streams.items.properties", map[string]any{
-		"enabled": M{
-			"type": "boolean",
-		},
-		"vars": M{
-			"type": "object",
-		},
-		"id": M{
-			"type": "string",
-		},
-		"compiled_stream": M{
-			"type": "object",
-		},
-		"data_stream": M{
-			"type": "object",
-			"properties": M{
-				"type": M{
-					"type": "string",
-				},
-				"dataset": M{
-					"type": "string",
-				},
-			},
-		},
-	})
-
-	// TODO: https://github.com/elastic/kibana/issues/151525
-	openapi.Delete("components.schemas.new_agent_policy.properties.unenroll_timeout.nullable")
-	openapi.Delete("components.schemas.new_agent_policy.properties.inactivity_timeout.nullable")
-
-	enc := json.NewEncoder(f)
-	enc.SetIndent("", "  ")
-	if err = enc.Encode(openapi); err != nil {
+	if err = os.WriteFile(*outFile, outData, 0664); err != nil {
 		log.Fatal(err)
 	}
 }
-
-func removeParametersKbnXsrf(m map[string]any) {
-	for k, v := range m {
-		if k == "parameters" {
-			if a, ok := v.([]interface{}); ok {
-				n := 0
-				for _, x := range a {
-					if keep(x) {
-						a[n] = x
-						n++
-					}
-				}
-				a = a[:n]
-
-				m[k] = a
-			}
-
-			continue
-		}
-
-		switch tmp := v.(type) {
-		case map[string]any:
-			removeParametersKbnXsrf(tmp)
-		case []any:
-			for _, item := range tmp {
-				if nestedMap, ok := item.(map[string]any); ok {
-					removeParametersKbnXsrf(nestedMap)
-				}
-			}
-		}
-	}
-}
-
-func keep(v interface{}) bool {
-	m, ok := v.(map[string]any)
-	if !ok {
-		return true
-	}
-
-	if ref, ok := m["$ref"].(string); ok && ref == "#/components/parameters/kbn_xsrf" {
-		return false
-	}
-
-	return true
-}
-
-type M map[string]any
-
-// Delete deletes the given key from the map.
-func (m M) Delete(key string) error {
-	k, d, _, found, err := mapFind(key, m, false)
-	if err != nil {
-		return err
-	}
-	if !found {
-		return ErrKeyNotFound
-	}
-
-	delete(d, k)
-	return nil
-}
-
-func (m M) Get(key string) interface{} {
-	_, _, v, found, err := mapFind(key, m, false)
-	if err != nil {
-		panic(err)
-	}
-	if !found {
-		return nil
-	}
-	return v
-}
-
-func (m M) Append(key string, value any) {
-	v := m.Get(key)
-	if v == nil {
-		panic(key + " not found for append.")
-	}
-	list, ok := v.([]interface{})
-	if !ok {
-		panic(fmt.Errorf("%v is not a list (got %T)", key, v))
-	}
-	list = append(list, value)
-	m.Put(key, list)
-}
-
-func (m M) Put(key string, value interface{}) (interface{}, error) {
-	// XXX `safemapstr.Put` mimics this implementation, both should be updated to have similar behavior
-	k, d, old, _, err := mapFind(key, m, true)
-	if err != nil {
-		return nil, err
-	}
-
-	d[k] = value
-	return old, nil
-}
-
-func mapFind(
-	key string,
-	data M,
-	createMissing bool,
-) (subKey string, subMap M, oldValue interface{}, present bool, err error) {
-	for {
-		// Fast path, key is present as is.
-		if v, exists := data[key]; exists {
-			return key, data, v, true, nil
-		}
-
-		idx := strings.IndexRune(key, '.')
-		if idx < 0 {
-			return key, data, nil, false, nil
-		}
-
-		k := key[:idx]
-		d, exists := data[k]
-		if !exists {
-			if createMissing {
-				d = M{}
-				data[k] = d
-			} else {
-				return "", nil, nil, false, ErrKeyNotFound
-			}
-		}
-
-		v, err := toMapStr(d)
-		if err != nil {
-			return "", nil, nil, false, err
-		}
-
-		// advance to sub-map
-		key = key[idx+1:]
-		data = v
-	}
-}
-
-func toMapStr(v interface{}) (M, error) {
-	m, ok := tryToMapStr(v)
-	if !ok {
-		return nil, fmt.Errorf("expected map but type is %T", v)
-	}
-	return m, nil
-}
-
-func tryToMapStr(v interface{}) (M, bool) {
-	switch m := v.(type) {
-	case M:
-		return m, true
-	case map[string]interface{}:
-		return M(m), true
-	default:
-		return nil, false
-	}
-}
-
-// ErrKeyNotFound indicates that the specified key was not found.
-var ErrKeyNotFound = errors.New("key not found")
