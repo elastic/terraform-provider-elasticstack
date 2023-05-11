858af1ef153cf3caad82039957b56e4c14fa8f57
Dmytro Onishchenko <dmytro.onishchenko@elastic.co>
2023-05-11 12:54:55 +0200

    acc test for email

--- internal/kibana/connector_test.go
+++ internal/kibana/connector_test.go
@@ -16,7 +16,66 @@ import (
 )
 
 func TestAccResourceKibanaConnectorCasesWebhook(t *testing.T) {
+	minSupportedVersion := version.Must(version.NewSemver("8.4.0"))
+
 	connectorName := sdkacctest.RandStringFromCharSet(22, sdkacctest.CharSetAlphaNum)
+
+	create := func(name string) string {
+		return fmt.Sprintf(`
+	provider "elasticstack" {
+	  elasticsearch {}
+	  kibana {}
+	}
+
+	resource "elasticstack_kibana_action_connector" "test" {
+	  name         = "%s"
+	  config       = jsonencode({
+		createIncidentJson = "{}"
+		createIncidentResponseKey = "key"
+		createIncidentUrl = "https://www.elastic.co/"
+		getIncidentResponseExternalTitleKey = "title"
+		getIncidentUrl = "https://www.elastic.co/"
+		updateIncidentJson = "{}"
+		updateIncidentUrl = "https://www.elastic.co/"
+		viewIncidentUrl = "https://www.elastic.co/"
+	  })
+	  secrets = jsonencode({
+		user = "test"
+		password = "test"
+	  })
+	  connector_type_id = ".cases-webhook"
+	}`,
+			name)
+	}
+
+	update := func(name string) string {
+		return fmt.Sprintf(`
+	provider "elasticstack" {
+	  elasticsearch {}
+	  kibana {}
+	}
+
+	resource "elasticstack_kibana_action_connector" "test" {
+	  name         = "Updated %s"
+	  config = jsonencode({
+		createIncidentJson = "{}"
+		createIncidentResponseKey = "key"
+		createIncidentUrl = "https://www.elastic.co/"
+		getIncidentResponseExternalTitleKey = "title"
+		getIncidentUrl = "https://www.elastic.co/"
+		updateIncidentJson = "{}"
+		updateIncidentUrl = "https://www.elastic.co/"
+		viewIncidentUrl = "https://www.elastic.co/"
+		createIncidentMethod = "put"
+	  })
+	  secrets = jsonencode({
+		user = "test"
+		password = "test"
+	  })
+	  connector_type_id = ".cases-webhook"
+	}`,
+			name)
+	}
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(t) },
@@ -24,8 +83,8 @@ func TestAccResourceKibanaConnectorCasesWebhook(t *testing.T) {
 		ProtoV5ProviderFactories: acctest.Providers,
 		Steps: []resource.TestStep{
 			{
-				SkipFunc: versionutils.CheckIfVersionIsUnsupported(version.Must(version.NewSemver("8.1.0"))),
-				Config:   testAccResourceKibanaConnectorCreateCasesWebhook(connectorName),
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
+				Config:   create(connectorName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", connectorName),
 					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".cases-webhook"),
@@ -36,7 +95,7 @@ func TestAccResourceKibanaConnectorCasesWebhook(t *testing.T) {
 			},
 			{
 				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
-				Config:   testAccResourceKibanaConnectorUpdateCasesWebhook(connectorName),
+				Config:   update(connectorName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", fmt.Sprintf("Updated %s", connectorName)),
 					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".cases-webhook"),
@@ -49,61 +108,155 @@ func TestAccResourceKibanaConnectorCasesWebhook(t *testing.T) {
 	})
 }
 
-func testAccResourceKibanaConnectorCreateCasesWebhook(name string) string {
-	return fmt.Sprintf(`
-provider "elasticstack" {
-  elasticsearch {}
-  kibana {}
+func TestAccResourceKibanaConnectorEmail(t *testing.T) {
+	minSupportedVersion := version.Must(version.NewSemver("7.14.0"))
+
+	connectorName := sdkacctest.RandStringFromCharSet(22, sdkacctest.CharSetAlphaNum)
+
+	create := func(name string) string {
+		return fmt.Sprintf(`
+	provider "elasticstack" {
+	  elasticsearch {}
+	  kibana {}
+	}
+
+	resource "elasticstack_kibana_action_connector" "test" {
+	  name         = "%s"
+	  config       = jsonencode({
+		from = "test@elastic.co"
+		port = 111
+		host = "localhost"
+		  })
+	  secrets = jsonencode({})
+	  connector_type_id = ".email"
+	}`,
+			name)
+	}
+
+	update := func(name string) string {
+		return fmt.Sprintf(`
+	provider "elasticstack" {
+	  elasticsearch {}
+	  kibana {}
+	}
+
+	resource "elasticstack_kibana_action_connector" "test" {
+	  name         = "Updated %s"
+	  config       = jsonencode({
+		from = "test2@elastic.co"
+		port = 222
+		host = "127.0.0.1"
+	  })
+	  secrets = jsonencode({
+		user = "user"
+		password = "password"
+	  })
+	  connector_type_id = ".email"
+	}`,
+			name)
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		CheckDestroy:             checkResourceKibanaConnectorDestroy,
+		ProtoV5ProviderFactories: acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
+				Config:   create(connectorName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", connectorName),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".email"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_deprecated", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_missing_secrets", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_preconfigured", "false"),
+				),
+			},
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
+				Config:   update(connectorName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", fmt.Sprintf("Updated %s", connectorName)),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".email"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_deprecated", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_missing_secrets", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_preconfigured", "false"),
+				),
+			},
+		},
+	})
 }
 
-resource "elasticstack_kibana_action_connector" "test" {
-  name         = "%s"
-  config       = jsonencode({
-    createIncidentJson = "{}"
-    createIncidentResponseKey = "key"
-    createIncidentUrl = "https://www.elastic.co/"
-    getIncidentResponseExternalTitleKey = "title"
-    getIncidentUrl = "https://www.elastic.co/"
-    updateIncidentJson = "{}"
-    updateIncidentUrl = "https://www.elastic.co/"
-    viewIncidentUrl = "https://www.elastic.co/"
-  })
-  secrets = jsonencode({
-    user = "test"
-    password = "test"
-  })
-  connector_type_id = ".cases-webhook"
-}
-	`, name)
-}
+func TestAccResourceKibanaConnectorIndex(t *testing.T) {
+	minSupportedVersion := version.Must(version.NewSemver("7.14.0"))
 
-func testAccResourceKibanaConnectorUpdateCasesWebhook(name string) string {
-	return fmt.Sprintf(`
-provider "elasticstack" {
-  elasticsearch {}
-  kibana {}
-}
+	connectorName := sdkacctest.RandStringFromCharSet(22, sdkacctest.CharSetAlphaNum)
 
-resource "elasticstack_kibana_action_connector" "test" {
-  name         = "Updated %s"
-  config = jsonencode({
-    createIncidentJson = "{}"
-    createIncidentResponseKey = "key"
-    createIncidentUrl = "https://www.elastic.co/"
-    getIncidentResponseExternalTitleKey = "title"
-    getIncidentUrl = "https://www.elastic.co/"
-    updateIncidentJson = "{}"
-    updateIncidentUrl = "https://www.elastic.co/"
-    viewIncidentUrl = "https://www.elastic.co/"
-    createIncidentMethod = "put"
-  })
-  secrets = jsonencode({
-    user = "test"
-    password = "test"
-  })
-  connector_type_id = ".cases-webhook"
-}
-	`, name)
+	create := func(name string) string {
+		return fmt.Sprintf(`
+	provider "elasticstack" {
+	  elasticsearch {}
+	  kibana {}
+	}
+
+	resource "elasticstack_kibana_action_connector" "test" {
+	  name         = "%s"
+	  config       = jsonencode({
+		index             = ".kibana"
+		refresh             = true
+	  })
+	  connector_type_id = ".index"
+	}`,
+			name)
+	}
+
+	update := func(name string) string {
+		return fmt.Sprintf(`
+	provider "elasticstack" {
+	  elasticsearch {}
+	  kibana {}
+	}
+
+	resource "elasticstack_kibana_action_connector" "test" {
+	  name         = "Updated %s"
+	  config       = jsonencode({
+		index             = ".kibana"
+		refresh             = false
+	  })
+	  connector_type_id = ".index"
+	}`,
+			name)
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		CheckDestroy:             checkResourceKibanaConnectorDestroy,
+		ProtoV5ProviderFactories: acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
+				Config:   create(connectorName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", connectorName),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".index"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_deprecated", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_missing_secrets", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_preconfigured", "false"),
+				),
+			},
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
+				Config:   update(connectorName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", fmt.Sprintf("Updated %s", connectorName)),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".index"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_deprecated", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_missing_secrets", "false"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_preconfigured", "false"),
+				),
+			},
+		},
+	})
 }
 
 func checkResourceKibanaConnectorDestroy(s *terraform.State) error {
@@ -128,74 +281,4 @@ func checkResourceKibanaConnectorDestroy(s *terraform.State) error {
 		}
 	}
 	return nil
-}
-
-func TestAccResourceKibanaConnectorIndex(t *testing.T) {
-	connectorName := sdkacctest.RandStringFromCharSet(22, sdkacctest.CharSetAlphaNum)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
-		CheckDestroy:             checkResourceKibanaConnectorDestroy,
-		ProtoV5ProviderFactories: acctest.Providers,
-		Steps: []resource.TestStep{
-			{
-				SkipFunc: versionutils.CheckIfVersionIsUnsupported(version.Must(version.NewSemver("7.14.0"))),
-				Config:   testAccResourceKibanaConnectorCreateIndex(connectorName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", connectorName),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".index"),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_deprecated", "false"),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_missing_secrets", "false"),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_preconfigured", "false"),
-				),
-			},
-			{
-				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedVersion),
-				Config:   testAccResourceKibanaConnectorUpdateIndex(connectorName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "name", fmt.Sprintf("Updated %s", connectorName)),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "connector_type_id", ".index"),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_deprecated", "false"),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_missing_secrets", "false"),
-					resource.TestCheckResourceAttr("elasticstack_kibana_action_connector.test", "is_preconfigured", "false"),
-				),
-			},
-		},
-	})
-}
-
-func testAccResourceKibanaConnectorCreateIndex(name string) string {
-	return fmt.Sprintf(`
-provider "elasticstack" {
-  elasticsearch {}
-  kibana {}
-}
-
-resource "elasticstack_kibana_action_connector" "test" {
-  name         = "%s"
-  config       = jsonencode({
-	index             = ".kibana"
-	refresh             = true
-  })
-  connector_type_id = ".index"
-}
-	`, name)
-}
-
-func testAccResourceKibanaConnectorUpdateIndex(name string) string {
-	return fmt.Sprintf(`
-provider "elasticstack" {
-  elasticsearch {}
-  kibana {}
-}
-
-resource "elasticstack_kibana_action_connector" "test" {
-  name         = "Updated %s"
-  config       = jsonencode({
-	index             = ".kibana"
-	refresh             = false
-  })
-  connector_type_id = ".index"
-}
-	`, name)
 }
