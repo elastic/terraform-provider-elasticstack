af55aefc4c1c266210a0988b880915d2ac2d2618
Taylor Swanson <taylor.swanson@elastic.co>
2023-10-25 16:15:23 -0500

    Change package test to skip_destroy
    - Resource is not unique and could cause unexpected test
    failures, or modify existing infrastructure in undesirable ways

--- internal/fleet/package_resource_test.go
+++ internal/fleet/package_resource_test.go
@@ -3,12 +3,10 @@ package fleet_test
 import (
 	"testing"
 
-	"github.com/hashicorp/go-version"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-
 	"github.com/elastic/terraform-provider-elasticstack/internal/acctest"
 	"github.com/elastic/terraform-provider-elasticstack/internal/versionutils"
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 var minVersionPackage = version.Must(version.NewVersion("8.6.0"))
@@ -20,16 +18,16 @@ provider "elasticstack" {
 }
 
 resource "elasticstack_fleet_package" "test_package" {
-  name    = "tcp"
-  version = "1.16.0"
-  force   = true
+  name         = "tcp"
+  version      = "1.16.0"
+  force        = true
+  skip_destroy = true
 }
 `
 
 func TestAccResourcePackage(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(t) },
-		CheckDestroy:             checkResourcePackageDestroy,
 		ProtoV5ProviderFactories: acctest.Providers,
 		Steps: []resource.TestStep{
 			{
@@ -42,8 +40,4 @@ func TestAccResourcePackage(t *testing.T) {
 			},
 		},
 	})
-}
-
-func checkResourcePackageDestroy(s *terraform.State) error {
-	return nil
 }
