e107f1c78871bd0b7b7351efd22072586aaa1414
SquareShark <oxo11@hotmail.co.uk>
2023-11-22 22:00:35 +0000

    testing commit verification

--- internal/clients/config/elasticsearch.go
+++ internal/clients/config/elasticsearch.go
@@ -229,7 +229,7 @@ func (c elasticsearchConfig) withEnvironmentOverrides() elasticsearchConfig {
 	}
 
 	if bearerToken := os.Getenv("ELASTICSEARCH_BEARER_TOKEN"); bearerToken != "" {
-		c.Header.Set("Authorization", bearerToken) // testing commit
+		c.Header.Set("Authorization", bearerToken)
 	}
 
 	if esClientAuthentication := os.Getenv("ELASTICSEARCH_BEARER_TOKEN"); esClientAuthentication != "" {
