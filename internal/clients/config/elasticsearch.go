3c4214fdb8da6764e38e229095d47b55aa76020f
SquareShark <oxo11@hotmail.co.uk>
2023-11-22 22:21:19 +0000

    testing verification

--- internal/clients/config/elasticsearch.go
+++ internal/clients/config/elasticsearch.go
@@ -229,7 +229,7 @@ func (c elasticsearchConfig) withEnvironmentOverrides() elasticsearchConfig {
 	}
 
 	if bearerToken := os.Getenv("ELASTICSEARCH_BEARER_TOKEN"); bearerToken != "" {
-		c.Header.Set("Authorization", bearerToken)
+		c.Header.Set("Authorization", bearerToken) // testing 2
 	}
 
 	if esClientAuthentication := os.Getenv("ELASTICSEARCH_BEARER_TOKEN"); esClientAuthentication != "" {
