31f1aa7a6a6856bca056b59a3215e20f3c68b3ca
Toby Brain <toby.brain@elastic.co>
2024-08-02 20:10:18 +1000

    Docs

--- examples/resources/elasticstack_elasticsearch_index/resource.tf
+++ examples/resources/elasticstack_elasticsearch_index/resource.tf
@@ -29,8 +29,7 @@ resource "elasticstack_elasticsearch_index" "my_index" {
     }
   })
 
-  number_of_shards      = 1
-  number_of_replicas    = 2
-  search_idle_after     = "20s"
-  total_shards_per_node = 200
+  number_of_shards   = 1
+  number_of_replicas = 2
+  search_idle_after  = "20s"
 }
