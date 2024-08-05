5742d9d9f9a748b67a65494eb13968de2e8e3eba
Boris Ilyushonak <boris.ilyushonak@elastic.co>
2024-08-05 17:42:34 +0200

    test optional params

--- internal/kibana/synthetics/private_location/acc_test.go
+++ internal/kibana/synthetics/private_location/acc_test.go
@@ -101,6 +101,69 @@ resource "elasticstack_kibana_synthetics_private_location" "test" {
 					resource.TestCheckResourceAttr(resourceId, "geo.lon", "-33.21"),
 				),
 			},
+			// Update and Read testing
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minKibanaVersion),
+				Config: testConfig("default", "test_policy_default") + `
+resource "elasticstack_kibana_synthetics_private_location" "test" {
+	label = "pl-test-label-2"
+	space_id = "default"
+	agent_policy_id = elasticstack_fleet_agent_policy.test_policy_default.policy_id
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceId, "label", "pl-test-label-2"),
+					resource.TestCheckResourceAttr(resourceId, "space_id", "default"),
+					resource.TestCheckResourceAttrSet(resourceId, "agent_policy_id"),
+					resource.TestCheckNoResourceAttr(resourceId, "tags"),
+					resource.TestCheckNoResourceAttr(resourceId, "geo"),
+				),
+			},
+			// Update and Read testing
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minKibanaVersion),
+				Config: testConfig("default", "test_policy_default") + `
+resource "elasticstack_kibana_synthetics_private_location" "test" {
+	label = "pl-test-label-2"
+	space_id = "default"
+	agent_policy_id = elasticstack_fleet_agent_policy.test_policy_default.policy_id
+	tags = ["c", "d", "e"]
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceId, "label", "pl-test-label-2"),
+					resource.TestCheckResourceAttr(resourceId, "space_id", "default"),
+					resource.TestCheckResourceAttrSet(resourceId, "agent_policy_id"),
+					resource.TestCheckResourceAttr(resourceId, "tags.#", "3"),
+					resource.TestCheckResourceAttr(resourceId, "tags.0", "c"),
+					resource.TestCheckResourceAttr(resourceId, "tags.1", "d"),
+					resource.TestCheckResourceAttr(resourceId, "tags.2", "e"),
+					resource.TestCheckNoResourceAttr(resourceId, "geo"),
+				),
+			},
+			// Update and Read testing
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minKibanaVersion),
+				Config: testConfig("default", "test_policy_default") + `
+resource "elasticstack_kibana_synthetics_private_location" "test" {
+	label = "pl-test-label-2"
+	space_id = "default"
+	agent_policy_id = elasticstack_fleet_agent_policy.test_policy_default.policy_id
+	geo = {
+		lat = -33.21
+		lon = -33.21
+	}
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceId, "label", "pl-test-label-2"),
+					resource.TestCheckResourceAttr(resourceId, "space_id", "default"),
+					resource.TestCheckResourceAttrSet(resourceId, "agent_policy_id"),
+					resource.TestCheckNoResourceAttr(resourceId, "tags"),
+					resource.TestCheckResourceAttr(resourceId, "geo.lat", "-33.21"),
+					resource.TestCheckResourceAttr(resourceId, "geo.lon", "-33.21"),
+				),
+			},
 			// Delete testing automatically occurs in TestCase
 		},
 	})
