d4007968e5e9bb69f23f08e6109d944cc5e96ba9
Sander van Vliet <117741489+sandermvanvliet-stack@users.noreply.github.com>
2024-08-08 06:16:09 +0200

    Support `restriction` in `elasticstack_elasticsearch_security_api_key` (#577)
    * Support restriction in elasticstack_elasticsearch_security_api_key
    
    * Update example in docs
    
    * Skip test for API key with restriction on any version below 8.9
    
    * Update docs
    
    * Raise error when restriction is not supported for API keys
    
    * Appease the linter
    
    * Small refactor
    
    * Revert debug change
    
    * Booleans are hard
    
    * This is a finicky test
    
    * Format a terraform example file
    
    * Use the ServerVersion() instead of creating a whole new request
    
    * Update docs
    
    * Remove two unused structs
    
    * Changelog
    
    ---------
    
    Co-authored-by: Toby Brain <toby.brain@elastic.co>

--- examples/resources/elasticstack_elasticsearch_security_api_key/resource.tf
+++ examples/resources/elasticstack_elasticsearch_security_api_key/resource.tf
@@ -26,6 +26,37 @@ resource "elasticstack_elasticsearch_security_api_key" "api_key" {
   })
 }
 
+# restriction on a role descriptor for an API key is supported since Elastic 8.9
+resource "elasticstack_elasticsearch_security_api_key" "api_key_with_restriction" {
+  # Set the name
+  name = "My API key"
+  # Set the role descriptors
+  role_descriptors = jsonencode({
+    role-a = {
+      cluster = ["all"],
+      indices = [
+        {
+          names      = ["index-a*"],
+          privileges = ["read"]
+        }
+      ],
+      restriction = {
+        workflows = ["search_application_query"]
+      }
+    }
+  })
+
+  # Set the expiration for the API key
+  expiration = "1d"
+
+  # Set the custom metadata for this user
+  metadata = jsonencode({
+    "env"    = "testing"
+    "open"   = false
+    "number" = 49
+  })
+}
+
 output "api_key" {
   value     = elasticstack_elasticsearch_security_api_key.api_key
   sensitive = true
