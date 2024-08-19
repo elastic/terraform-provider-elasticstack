7773917d765725ead98b29122a79027f9c75e8d2
jdesnoes <126451335+jdesnoes@users.noreply.github.com>
2024-08-19 10:12:23 +0200

    Kibana spaces data source (#682)
    * Spaces Data Source
    
    * Bump github.com/hashicorp/terraform-plugin-framework (#679)
    
    Bumps [github.com/hashicorp/terraform-plugin-framework](https://github.com/hashicorp/terraform-plugin-framework) from 1.9.0 to 1.10.0.
    - [Release notes](https://github.com/hashicorp/terraform-plugin-framework/releases)
    - [Changelog](https://github.com/hashicorp/terraform-plugin-framework/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/hashicorp/terraform-plugin-framework/compare/v1.9.0...v1.10.0)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/hashicorp/terraform-plugin-framework
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Spaces data source
    
    * Spaces data source
    
    * Kibana spaces data source
    
    * Kibana spaces data source
    
    * Kibana spaces data source
    
    * Kibana spaces data source
    
    * Kibana spaces data source
    
    * Kibana spaces data source
    
    * Suggestions apply
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ internal/kibana/spaces/read.go
@@ -0,0 +1,51 @@
+package spaces
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Read refreshes the Terraform state with the latest data.
+func (d *dataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state dataSourceModel
+
+	// Call client API
+	spaces, err := d.client.List()
+	if err != nil {
+		resp.Diagnostics.AddError("unable to list spaces", err.Error())
+		return
+	}
+
+	// Map response body to model
+	for _, space := range spaces {
+		spaceState := model{
+			ID:          types.StringValue(space.ID),
+			Name:        types.StringValue(space.Name),
+			Description: types.StringValue(space.Description),
+			Initials:    types.StringValue(space.Initials),
+			Color:       types.StringValue(space.Color),
+			ImageUrl:    types.StringValue(space.ImageURL),
+		}
+
+		disabledFeatures, diags := types.ListValueFrom(ctx, types.StringType, space.DisabledFeatures)
+		if diags.HasError() {
+			resp.Diagnostics.Append(diags...)
+			return
+		}
+
+		spaceState.DisabledFeatures = disabledFeatures
+
+		state.Spaces = append(state.Spaces, spaceState)
+	}
+
+	state.ID = types.StringValue("spaces")
+
+	// Set state
+	diags := resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
--- provider/plugin_framework.go
+++ provider/plugin_framework.go
@@ -7,6 +7,7 @@ import (
 	"github.com/elastic/terraform-provider-elasticstack/internal/clients/config"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/data_view"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/import_saved_objects"
+	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/spaces"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/synthetics/private_location"
 	"github.com/elastic/terraform-provider-elasticstack/internal/schema"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
@@ -65,7 +66,9 @@ func (p *Provider) Configure(ctx context.Context, req fwprovider.ConfigureReques
 }
 
 func (p *Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{}
+	return []func() datasource.DataSource{
+		spaces.NewDataSource,
+	}
 }
 
 func (p *Provider) Resources(ctx context.Context) []func() resource.Resource {
