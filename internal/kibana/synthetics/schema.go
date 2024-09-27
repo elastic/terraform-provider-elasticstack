13544c4606b8ef0756e6c8997df533e8b741f7d6
Boris Ilyushonak <boris.ilyushonak@elastic.co>
2024-09-27 14:25:20 +0200

    fix: synthetics http monitor creations produced inconsistent result after apply
     for https://github.com/elastic/terraform-provider-elasticstack/issues/800
     fix for the fileds: http.locations, http.proxy_url, http.ssl_supported_protocols, http.ssl_verification_mode, tcp.proxy_url, tcp.ssl_supported_protocols, tcp.ssl_verification_mode

--- internal/kibana/synthetics/schema.go
+++ internal/kibana/synthetics/schema.go
@@ -1,6 +1,7 @@
 package synthetics
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/disaster37/go-kibana-rest/v8/kbapi"
@@ -9,10 +10,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -41,7 +44,7 @@ type tfAlertConfigV0 struct {
 type tfHTTPMonitorFieldsV0 struct {
 	URL                   types.String         `tfsdk:"url"`
 	SslVerificationMode   types.String         `tfsdk:"ssl_verification_mode"`
-	SslSupportedProtocols []types.String       `tfsdk:"ssl_supported_protocols"`
+	SslSupportedProtocols types.List           `tfsdk:"ssl_supported_protocols"`
 	MaxRedirects          types.Int64          `tfsdk:"max_redirects"`
 	Mode                  types.String         `tfsdk:"mode"`
 	IPv4                  types.Bool           `tfsdk:"ipv4"`
@@ -55,13 +58,13 @@ type tfHTTPMonitorFieldsV0 struct {
 }
 
 type tfTCPMonitorFieldsV0 struct {
-	Host                  types.String   `tfsdk:"host"`
-	SslVerificationMode   types.String   `tfsdk:"ssl_verification_mode"`
-	SslSupportedProtocols []types.String `tfsdk:"ssl_supported_protocols"`
-	CheckSend             types.String   `tfsdk:"check_send"`
-	CheckReceive          types.String   `tfsdk:"check_receive"`
-	ProxyURL              types.String   `tfsdk:"proxy_url"`
-	ProxyUseLocalResolver types.Bool     `tfsdk:"proxy_use_local_resolver"`
+	Host                  types.String `tfsdk:"host"`
+	SslVerificationMode   types.String `tfsdk:"ssl_verification_mode"`
+	SslSupportedProtocols types.List   `tfsdk:"ssl_supported_protocols"`
+	CheckSend             types.String `tfsdk:"check_send"`
+	CheckReceive          types.String `tfsdk:"check_receive"`
+	ProxyURL              types.String `tfsdk:"proxy_url"`
+	ProxyUseLocalResolver types.Bool   `tfsdk:"proxy_use_local_resolver"`
 }
 
 type tfICMPMonitorFieldsV0 struct {
@@ -293,11 +296,15 @@ func httpMonitorFieldsSchema() schema.Attribute {
 			"ssl_verification_mode": schema.StringAttribute{
 				Optional:            true,
 				MarkdownDescription: "Controls the verification of server certificates. ",
+				Computed:            true,
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 			},
 			"ssl_supported_protocols": schema.ListAttribute{
 				ElementType:         types.StringType,
 				Optional:            true,
 				MarkdownDescription: "List of allowed SSL/TLS versions.",
+				Computed:            true,
+				PlanModifiers:       []planmodifier.List{listplanmodifier.UseStateForUnknown()},
 			},
 			"max_redirects": schema.Int64Attribute{
 				Optional:            true,
@@ -332,6 +339,8 @@ func httpMonitorFieldsSchema() schema.Attribute {
 			"proxy_url": schema.StringAttribute{
 				Optional:            true,
 				MarkdownDescription: "The URL of the proxy to use for this monitor.",
+				Computed:            true,
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 			},
 			"response": jsonObjectSchema("Controls the indexing of the HTTP response body contents to the `http.response.body.contents` field."),
 			"check":    jsonObjectSchema("The check request settings."),
@@ -352,11 +361,15 @@ func tcpMonitorFieldsSchema() schema.Attribute {
 			"ssl_verification_mode": schema.StringAttribute{
 				Optional:            true,
 				MarkdownDescription: "Controls the verification of server certificates. ",
+				Computed:            true,
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 			},
 			"ssl_supported_protocols": schema.ListAttribute{
 				ElementType:         types.StringType,
 				Optional:            true,
 				MarkdownDescription: "List of allowed SSL/TLS versions.",
+				Computed:            true,
+				PlanModifiers:       []planmodifier.List{listplanmodifier.UseStateForUnknown()},
 			},
 			"check_send": schema.StringAttribute{
 				Optional:            true,
@@ -369,6 +382,8 @@ func tcpMonitorFieldsSchema() schema.Attribute {
 			"proxy_url": schema.StringAttribute{
 				Optional:            true,
 				MarkdownDescription: "The URL of the SOCKS5 proxy to use when connecting to the server. The value must be a URL with a scheme of `socks5://`. If the SOCKS5 proxy server requires client authentication, then a username and password can be embedded in the URL. When using a proxy, hostnames are resolved on the proxy server instead of on the client. You can change this behavior by setting the `proxy_use_local_resolver` option.",
+				Computed:            true,
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 			},
 			"proxy_use_local_resolver": schema.BoolAttribute{
 				Optional:            true,
@@ -435,6 +450,19 @@ func StringSliceValue(v []string) []types.String {
 	return res
 }
 
+func StringSliceToListAttrValue(v []string) (types.List, diag.Diagnostics) {
+
+	if v == nil {
+		return types.ListUnknown(types.StringType), diag.Diagnostics{}
+	}
+
+	var res []attr.Value
+	for _, s := range v {
+		res = append(res, types.StringValue(s))
+	}
+	return types.ListValue(types.StringType, res)
+}
+
 func toNormalizedValue(jsObj kbapi.JsonObject) (jsontypes.Normalized, error) {
 	res, err := json.Marshal(jsObj)
 	if err != nil {
@@ -464,20 +492,22 @@ func stringToInt64(v string) (int64, error) {
 	return res, err
 }
 
-func (v *tfModelV0) toModelV0(api *kbapi.SyntheticsMonitor) (*tfModelV0, error) {
+func (v *tfModelV0) toModelV0(api *kbapi.SyntheticsMonitor) (*tfModelV0, diag.Diagnostics) {
 	var schedule int64
 	var err error
+	dg := diag.Diagnostics{}
 	if api.Schedule != nil {
 		schedule, err = stringToInt64(api.Schedule.Number)
 		if err != nil {
-			return nil, err
+			dg.AddError("Failed to convert schedule to int64", err.Error())
+			return nil, dg
 		}
 	}
 	var locLabels []string
 	var privateLocLabels []string
 	for _, l := range api.Locations {
 		if l.IsServiceManaged {
-			locLabels = append(locLabels, l.Label)
+			locLabels = append(locLabels, l.Id)
 		} else {
 			privateLocLabels = append(privateLocLabels, l.Label)
 		}
@@ -485,7 +515,8 @@ func (v *tfModelV0) toModelV0(api *kbapi.SyntheticsMonitor) (*tfModelV0, error) 
 
 	timeout, err := stringToInt64(string(api.Timeout))
 	if err != nil {
-		return nil, err
+		dg.AddError("Failed to convert timeout to int64", err.Error())
+		return nil, dg
 	}
 
 	var http *tfHTTPMonitorFieldsV0
@@ -499,13 +530,13 @@ func (v *tfModelV0) toModelV0(api *kbapi.SyntheticsMonitor) (*tfModelV0, error) 
 		if v.HTTP != nil {
 			http = v.HTTP
 		}
-		http, err = http.toTfHTTPMonitorFieldsV0(api)
+		http = http.toTfHTTPMonitorFieldsV0(dg, api)
 	case kbapi.Tcp:
 		tcp = &tfTCPMonitorFieldsV0{}
 		if v.TCP != nil {
 			tcp = v.TCP
 		}
-		tcp, err = tcp.toTfTCPMonitorFieldsV0(api)
+		tcp = tcp.toTfTCPMonitorFieldsV0(dg, api)
 	case kbapi.Icmp:
 		icmp = &tfICMPMonitorFieldsV0{}
 		if v.ICMP != nil {
@@ -523,14 +554,16 @@ func (v *tfModelV0) toModelV0(api *kbapi.SyntheticsMonitor) (*tfModelV0, error) 
 	}
 
 	if err != nil {
-		return nil, err
+		dg.AddError("Failed to convert monitor fields", err.Error())
+		return nil, dg
 	}
 
 	params := v.Params
 	if api.Params != nil {
 		params, err = toNormalizedValue(api.Params)
 		if err != nil {
-			return nil, err
+			dg.AddError("Failed to parse params", err.Error())
+			return nil, dg
 		}
 	}
 
@@ -557,10 +590,10 @@ func (v *tfModelV0) toModelV0(api *kbapi.SyntheticsMonitor) (*tfModelV0, error) 
 		ICMP:             icmp,
 		Browser:          browser,
 		RetestOnFailure:  v.RetestOnFailure,
-	}, nil
+	}, dg
 }
 
-func (v *tfTCPMonitorFieldsV0) toTfTCPMonitorFieldsV0(api *kbapi.SyntheticsMonitor) (*tfTCPMonitorFieldsV0, error) {
+func (v *tfTCPMonitorFieldsV0) toTfTCPMonitorFieldsV0(dg diag.Diagnostics, api *kbapi.SyntheticsMonitor) *tfTCPMonitorFieldsV0 {
 	checkSend := v.CheckSend
 	if api.CheckSend != "" {
 		checkSend = types.StringValue(api.CheckSend)
@@ -569,15 +602,20 @@ func (v *tfTCPMonitorFieldsV0) toTfTCPMonitorFieldsV0(api *kbapi.SyntheticsMonit
 	if api.CheckReceive != "" {
 		checkReceive = types.StringValue(api.CheckReceive)
 	}
+	sslSupportedProtocols, d := StringSliceToListAttrValue(api.SslSupportedProtocols)
+	dg.Append(d...)
+	if d.HasError() {
+		return nil
+	}
 	return &tfTCPMonitorFieldsV0{
 		Host:                  types.StringValue(api.Host),
 		SslVerificationMode:   types.StringValue(api.SslVerificationMode),
-		SslSupportedProtocols: StringSliceValue(api.SslSupportedProtocols),
+		SslSupportedProtocols: sslSupportedProtocols,
 		CheckSend:             checkSend,
 		CheckReceive:          checkReceive,
 		ProxyURL:              types.StringValue(api.ProxyUrl),
 		ProxyUseLocalResolver: types.BoolPointerValue(api.ProxyUseLocalResolver),
-	}, nil
+	}
 }
 
 func (v *tfICMPMonitorFieldsV0) toTfICMPMonitorFieldsV0(api *kbapi.SyntheticsMonitor) (*tfICMPMonitorFieldsV0, error) {
@@ -621,14 +659,15 @@ func (v *tfBrowserMonitorFieldsV0) toTfBrowserMonitorFieldsV0(api *kbapi.Synthet
 	}, nil
 }
 
-func (v *tfHTTPMonitorFieldsV0) toTfHTTPMonitorFieldsV0(api *kbapi.SyntheticsMonitor) (*tfHTTPMonitorFieldsV0, error) {
+func (v *tfHTTPMonitorFieldsV0) toTfHTTPMonitorFieldsV0(dg diag.Diagnostics, api *kbapi.SyntheticsMonitor) *tfHTTPMonitorFieldsV0 {
 
 	var err error
 	proxyHeaders := v.ProxyHeader
 	if api.ProxyHeaders != nil {
 		proxyHeaders, err = toNormalizedValue(api.ProxyHeaders)
 		if err != nil {
-			return nil, err
+			dg.AddError("Failed to parse proxy_headers", err.Error())
+			return nil
 		}
 	}
 
@@ -643,13 +682,19 @@ func (v *tfHTTPMonitorFieldsV0) toTfHTTPMonitorFieldsV0(api *kbapi.SyntheticsMon
 
 	maxRedirects, err := stringToInt64(api.MaxRedirects)
 	if err != nil {
-		return nil, err
+		dg.AddError("Failed to parse max_redirects", err.Error())
+		return nil
 	}
 
+	sslSupportedProtocols, d := StringSliceToListAttrValue(api.SslSupportedProtocols)
+	dg.Append(d...)
+	if d.HasError() {
+		return nil
+	}
 	return &tfHTTPMonitorFieldsV0{
 		URL:                   types.StringValue(api.Url),
 		SslVerificationMode:   types.StringValue(api.SslVerificationMode),
-		SslSupportedProtocols: StringSliceValue(api.SslSupportedProtocols),
+		SslSupportedProtocols: sslSupportedProtocols,
 		MaxRedirects:          types.Int64Value(maxRedirects),
 		Mode:                  types.StringValue(string(api.Mode)),
 		IPv4:                  types.BoolPointerValue(api.Ipv4),
@@ -660,7 +705,7 @@ func (v *tfHTTPMonitorFieldsV0) toTfHTTPMonitorFieldsV0(api *kbapi.SyntheticsMon
 		ProxyURL:              types.StringValue(api.ProxyUrl),
 		Check:                 v.Check,
 		Response:              v.Response,
-	}, nil
+	}
 }
 
 func toTfAlertConfigV0(alert *kbapi.MonitorAlertConfig) *tfAlertConfigV0 {
@@ -682,9 +727,9 @@ func toTfStatusConfigV0(status *kbapi.SyntheticsStatusConfig) *tfStatusConfigV0 
 	}
 }
 
-func (v *tfModelV0) toKibanaAPIRequest() (*kibanaAPIRequest, diag.Diagnostics) {
+func (v *tfModelV0) toKibanaAPIRequest(ctx context.Context) (*kibanaAPIRequest, diag.Diagnostics) {
 
-	fields, dg := v.toMonitorFields()
+	fields, dg := v.toMonitorFields(ctx)
 	if dg.HasError() {
 		return nil, dg
 	}
@@ -698,13 +743,13 @@ func (v *tfModelV0) toKibanaAPIRequest() (*kibanaAPIRequest, diag.Diagnostics) {
 	}, dg
 }
 
-func (v *tfModelV0) toMonitorFields() (kbapi.MonitorFields, diag.Diagnostics) {
+func (v *tfModelV0) toMonitorFields(ctx context.Context) (kbapi.MonitorFields, diag.Diagnostics) {
 	var dg diag.Diagnostics
 
 	if v.HTTP != nil {
-		return v.toHttpMonitorFields()
+		return v.toHttpMonitorFields(ctx)
 	} else if v.TCP != nil {
-		return v.toTCPMonitorFields(), dg
+		return v.toTCPMonitorFields(ctx)
 	} else if v.ICMP != nil {
 		return v.toICMPMonitorFields(), dg
 	} else if v.Browser != nil {
@@ -751,7 +796,7 @@ func tfInt64ToString(v types.Int64) string {
 	return res
 }
 
-func (v *tfModelV0) toHttpMonitorFields() (kbapi.MonitorFields, diag.Diagnostics) {
+func (v *tfModelV0) toHttpMonitorFields(ctx context.Context) (kbapi.MonitorFields, diag.Diagnostics) {
 	proxyHeaders, dg := toJsonObject(v.HTTP.ProxyHeader)
 	if dg.HasError() {
 		return nil, dg
@@ -764,11 +809,14 @@ func (v *tfModelV0) toHttpMonitorFields() (kbapi.MonitorFields, diag.Diagnostics
 	if dg.HasError() {
 		return nil, dg
 	}
+
+	sslSupportedProtocols, dg := tfListToStringSlice(ctx, v.HTTP.SslSupportedProtocols)
+
 	maxRedirects := tfInt64ToString(v.HTTP.MaxRedirects)
 	return kbapi.HTTPMonitorFields{
 		Url:                   v.HTTP.URL.ValueString(),
 		SslVerificationMode:   v.HTTP.SslVerificationMode.ValueString(),
-		SslSupportedProtocols: ValueStringSlice(v.HTTP.SslSupportedProtocols),
+		SslSupportedProtocols: sslSupportedProtocols,
 		MaxRedirects:          maxRedirects,
 		Mode:                  kbapi.HttpMonitorMode(v.HTTP.Mode.ValueString()),
 		Ipv4:                  v.HTTP.IPv4.ValueBoolPointer(),
@@ -782,16 +830,29 @@ func (v *tfModelV0) toHttpMonitorFields() (kbapi.MonitorFields, diag.Diagnostics
 	}, diag.Diagnostics{} //dg
 }
 
-func (v *tfModelV0) toTCPMonitorFields() kbapi.MonitorFields {
+func tfListToStringSlice(ctx context.Context, v types.List) ([]string, diag.Diagnostics) {
+	var res []string
+	if !v.IsNull() {
+		if d := v.ElementsAs(ctx, &res, true); d.HasError() {
+			return nil, d
+		}
+	}
+
+	return res, diag.Diagnostics{}
+}
+
+func (v *tfModelV0) toTCPMonitorFields(ctx context.Context) (kbapi.MonitorFields, diag.Diagnostics) {
+	sslSupportedProtocols, dg := tfListToStringSlice(ctx, v.TCP.SslSupportedProtocols)
+
 	return kbapi.TCPMonitorFields{
 		Host:                  v.TCP.Host.ValueString(),
 		SslVerificationMode:   v.TCP.SslVerificationMode.ValueString(),
-		SslSupportedProtocols: ValueStringSlice(v.TCP.SslSupportedProtocols),
+		SslSupportedProtocols: sslSupportedProtocols,
 		CheckSend:             v.TCP.CheckSend.ValueString(),
 		CheckReceive:          v.TCP.CheckReceive.ValueString(),
 		ProxyUrl:              v.TCP.ProxyURL.ValueString(),
 		ProxyUseLocalResolver: v.TCP.ProxyUseLocalResolver.ValueBoolPointer(),
-	}
+	}, dg
 }
 
 func (v *tfModelV0) toICMPMonitorFields() kbapi.MonitorFields {
