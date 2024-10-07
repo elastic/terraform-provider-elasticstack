e78d2c0ea0c12f7f05dadab5443bff391cc40428
Ott Ramst <ott.ramst@gmail.com>
2024-10-07 14:25:18 +0300

    Add version constraints to tests

--- internal/elasticsearch/transform/transform_test.go
+++ internal/elasticsearch/transform/transform_test.go
@@ -6,10 +6,14 @@ import (
 
 	"github.com/elastic/terraform-provider-elasticstack/internal/acctest"
 	"github.com/elastic/terraform-provider-elasticstack/internal/clients"
+	"github.com/elastic/terraform-provider-elasticstack/internal/versionutils"
+	"github.com/hashicorp/go-version"
 	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
+
+var minSupportedDestAliasesVersion = version.Must(version.NewSemver("8.8.0"))
 
 func TestAccResourceTransformWithPivot(t *testing.T) {
 
@@ -112,7 +116,8 @@ func TestAccResourceTransformWithAliases(t *testing.T) {
 		ProtoV6ProviderFactories: acctest.Providers,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceTransformWithAliases(transformName),
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedDestAliasesVersion),
+				Config:   testAccResourceTransformWithAliasesCreate(transformName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "name", transformName),
 					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.#", "2"),
@@ -120,6 +125,18 @@ func TestAccResourceTransformWithAliases(t *testing.T) {
 					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.0.move_on_creation", "true"),
 					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.1.alias", "test_alias_2"),
 					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.1.move_on_creation", "false"),
+				),
+			},
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minSupportedDestAliasesVersion),
+				Config:   testAccResourceTransformWithAliasesUpdate(transformName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "name", transformName),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.#", "3"),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.0.alias", "test_alias_1"),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.0.move_on_creation", "false"),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.1.alias", "test_alias_2"),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_transform.test_aliases", "destination.0.aliases.1.move_on_creation", "true"),
 				),
 			},
 		},
@@ -379,7 +396,7 @@ resource "elasticstack_elasticsearch_transform" "test_pivot" {
   `, indexName, transformName)
 }
 
-func testAccResourceTransformWithAliases(name string) string {
+func testAccResourceTransformWithAliasesCreate(name string) string {
 	return fmt.Sprintf(`
 provider "elasticstack" {
   elasticsearch {}
@@ -429,6 +446,60 @@ resource "elasticstack_elasticsearch_transform" "test_aliases" {
 `, name)
 }
 
+func testAccResourceTransformWithAliasesUpdate(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  elasticsearch {}
+}
+
+resource "elasticstack_elasticsearch_transform" "test_aliases" {
+  name = "%s"
+  description = "test transform with aliases"
+
+  source {
+    indices = ["source_index"]
+  }
+
+  destination {
+    index = "dest_index_for_transform"
+
+    aliases {
+      alias = "test_alias_1"
+      move_on_creation = false
+    }
+
+    aliases {
+      alias = "test_alias_2"
+      move_on_creation = true
+    }
+
+	aliases {
+      alias = "test_alias_3"
+	}
+  }
+
+  pivot = jsonencode({
+    "group_by": {
+      "customer_id": {
+        "terms": {
+          "field": "customer_id"
+        }
+      }
+    },
+    "aggregations": {
+      "total_sales": {
+        "sum": {
+          "field": "sales"
+        }
+      }
+    }
+  })
+
+  defer_validation = true
+}
+`, name)
+}
+
 func checkResourceTransformDestroy(s *terraform.State) error {
 	client, err := clients.NewAcceptanceTestingClient()
 	if err != nil {
@@ -452,7 +523,7 @@ func checkResourceTransformDestroy(s *terraform.State) error {
 		}
 
 		if res.StatusCode != 404 {
-			return fmt.Errorf("Transform (%s) still exists", compId.ResourceId)
+			return fmt.Errorf("transform (%s) still exists", compId.ResourceId)
 		}
 	}
 	return nil
