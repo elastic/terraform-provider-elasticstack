284a2e2ee2e864a643d4fe5400ba2563a6c438d0
Toby Brain <toby.brain@elastic.co>
2025-06-04 15:24:04 +1000

    Add acctest to cover sdk->pf migration

--- internal/elasticsearch/security/system_user/acc_test.go
+++ internal/elasticsearch/security/system_user/acc_test.go
@@ -44,6 +44,37 @@ func TestAccResourceSecuritySystemUserNotFound(t *testing.T) {
 	})
 }
 
+func TestAccResourceSecuritySystemUserFromSDK(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				// Create the system user with the last provider version where the system user resource was built on the SDK
+				ExternalProviders: map[string]resource.ExternalProvider{
+					"elasticstack": {
+						Source:            "elastic/elasticstack",
+						VersionConstraint: "0.11.15",
+					},
+				},
+				ProtoV6ProviderFactories: acctest.Providers,
+				Config:                   testAccResourceSecuritySystemUserCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_security_system_user.remote_monitoring_user", "username", "remote_monitoring_user"),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_security_system_user.remote_monitoring_user", "enabled", "true"),
+				),
+			},
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				Config:                   testAccResourceSecuritySystemUserCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_security_system_user.remote_monitoring_user", "username", "remote_monitoring_user"),
+					resource.TestCheckResourceAttr("elasticstack_elasticsearch_security_system_user.remote_monitoring_user", "enabled", "true"),
+				),
+			},
+		},
+	})
+}
+
 const testAccResourceSecuritySystemUserCreate = `
 provider "elasticstack" {
   elasticsearch {}
