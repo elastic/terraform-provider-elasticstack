3ecd775e33b13fb17007df2d80c80049a98ee87a
Toby Brain <toby.brain@elastic.co>
2025-08-22 20:47:58 +1000

    Don't force replacement when changing integration versions

--- internal/fleet/integration/resource_test.go
+++ internal/fleet/integration/resource_test.go
@@ -2,6 +2,7 @@ package integration_test
 
 import (
 	"context"
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -10,11 +11,15 @@ import (
 	"github.com/elastic/terraform-provider-elasticstack/internal/clients/fleet"
 	"github.com/elastic/terraform-provider-elasticstack/internal/versionutils"
 	"github.com/hashicorp/go-version"
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/stretchr/testify/require"
 )
 
-var minVersionIntegration = version.Must(version.NewVersion("8.6.0"))
+var (
+	minVersionIntegration       = version.Must(version.NewVersion("8.6.0"))
+	minVersionIntegrationPolicy = version.Must(version.NewVersion("8.10.0"))
+)
 
 func TestAccResourceIntegrationFromSDK(t *testing.T) {
 	resource.Test(t, resource.TestCase{
@@ -72,6 +77,40 @@ func TestAccResourceIntegration(t *testing.T) {
 	})
 }
 
+func TestAccResourceIntegrationWithPolicy(t *testing.T) {
+	policyName := sdkacctest.RandStringFromCharSet(22, sdkacctest.CharSetAlphaNum)
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		Steps: []resource.TestStep{
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minVersionIntegrationPolicy),
+				Config:   testAccResourceIntegrationWithPolicy(policyName, "1.16.0"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_fleet_integration.test_integration", "name", "tcp"),
+					resource.TestCheckResourceAttr("elasticstack_fleet_integration.test_integration", "version", "1.16.0"),
+				),
+			},
+			{
+				SkipFunc: versionutils.CheckIfVersionIsUnsupported(minVersionIntegrationPolicy),
+				Config:   testAccResourceIntegrationWithPolicy(policyName, "1.17.0"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("elasticstack_fleet_integration.test_integration", "name", "tcp"),
+					resource.TestCheckResourceAttr("elasticstack_fleet_integration.test_integration", "version", "1.17.0"),
+				),
+			},
+			{
+				SkipFunc:          versionutils.CheckIfVersionIsUnsupported(minVersionIntegrationPolicy),
+				ResourceName:      "elasticstack_fleet_integration.test_integration",
+				Config:            testAccResourceIntegrationWithPolicy(policyName, "1.17.0"),
+				ImportState:       true,
+				ImportStateVerify: true,
+				ExpectError:       regexp.MustCompile("Resource Import Not Implemented"),
+			},
+		},
+	})
+}
+
 func TestAccResourceIntegrationDeleted(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(t) },
@@ -121,6 +160,65 @@ resource "elasticstack_fleet_integration" "test_integration" {
   skip_destroy = true
 }
 `
+
+func testAccResourceIntegrationWithPolicy(policyName, version string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  elasticsearch {}
+  kibana {}
+}
+
+resource "elasticstack_fleet_integration" "test_integration" {
+  name         = "tcp"
+  version      = "%s"
+  force        = true
+  skip_destroy = true
+}
+
+// An agent policy to hold the integration policy.
+resource "elasticstack_fleet_agent_policy" "sample" {
+  name            = "%s"
+  namespace       = "default"
+  description     = "A sample agent policy"
+  monitor_logs    = true
+  monitor_metrics = true
+  skip_destroy    = false
+}
+
+// The associated enrollment token.
+data "elasticstack_fleet_enrollment_tokens" "sample" {
+  policy_id = elasticstack_fleet_agent_policy.sample.policy_id
+}
+
+// The integration policy.
+resource "elasticstack_fleet_integration_policy" "sample" {
+  name                = "%s"
+  namespace           = "default"
+  description         = "A sample integration policy"
+  agent_policy_id     = elasticstack_fleet_agent_policy.sample.policy_id
+  integration_name    = elasticstack_fleet_integration.test_integration.name
+  integration_version = elasticstack_fleet_integration.test_integration.version
+
+  input {
+    input_id = "tcp-tcp"
+    streams_json = jsonencode({
+      "tcp.generic" : {
+        "enabled" : true,
+        "vars" : {
+          "listen_address" : "localhost",
+          "listen_port" : 8080,
+          "data_stream.dataset" : "tcp.generic",
+          "tags" : [],
+          "syslog_options" : "field: message\n#format: auto\n#timezone: Local\n",
+          "ssl" : "#certificate: |\n#    -----BEGIN CERTIFICATE-----\n#    ...\n#    -----END CERTIFICATE-----\n#key: |\n#    -----BEGIN PRIVATE KEY-----\n#    ...\n#    -----END PRIVATE KEY-----\n",
+          "custom" : ""
+        }
+      }
+    })
+  }
+}
+`, version, policyName, policyName)
+}
 
 const testAccResourceIntegrationDeleted = `
 provider "elasticstack" {
