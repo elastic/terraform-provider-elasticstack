ef054eba13b6ed68c22373e5b5fbb479a23463ba
Nick Benoit <nick.benoit@elastic.co>
2025-09-15 11:12:25 -0600

    Add basic acceptance tests for all rule types

--- internal/kibana/security_detection_rule/acc_test.go
+++ internal/kibana/security_detection_rule/acc_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
-func TestAccResourceSecurityDetectionRule(t *testing.T) {
+func TestAccResourceSecurityDetectionRule_Query(t *testing.T) {
 	resourceName := "elasticstack_kibana_security_detection_rule.test"
 
 	resource.Test(t, resource.TestCase{
@@ -23,16 +23,17 @@ func TestAccResourceSecurityDetectionRule(t *testing.T) {
 		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSecurityDetectionRuleConfig_basic("test-rule"),
+				Config: testAccSecurityDetectionRuleConfig_query("test-query-rule"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", "test-rule"),
+					resource.TestCheckResourceAttr(resourceName, "name", "test-query-rule"),
 					resource.TestCheckResourceAttr(resourceName, "type", "query"),
 					resource.TestCheckResourceAttr(resourceName, "query", "*:*"),
 					resource.TestCheckResourceAttr(resourceName, "language", "kuery"),
 					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "description", "Test security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test query security detection rule"),
 					resource.TestCheckResourceAttr(resourceName, "severity", "medium"),
 					resource.TestCheckResourceAttr(resourceName, "risk_score", "50"),
+					resource.TestCheckResourceAttr(resourceName, "index.0", "logs-*"),
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
 					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
@@ -40,12 +41,215 @@ func TestAccResourceSecurityDetectionRule(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccSecurityDetectionRuleConfig_update("test-rule-updated"),
+				Config: testAccSecurityDetectionRuleConfig_queryUpdate("test-query-rule-updated"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", "test-rule-updated"),
-					resource.TestCheckResourceAttr(resourceName, "description", "Updated test security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "name", "test-query-rule-updated"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Updated test query security detection rule"),
 					resource.TestCheckResourceAttr(resourceName, "severity", "high"),
 					resource.TestCheckResourceAttr(resourceName, "risk_score", "75"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_EQL(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_eql("test-eql-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-eql-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "eql"),
+					resource.TestCheckResourceAttr(resourceName, "query", "process where process.name == \"cmd.exe\""),
+					resource.TestCheckResourceAttr(resourceName, "language", "eql"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test EQL security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "high"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "70"),
+					resource.TestCheckResourceAttr(resourceName, "index.0", "winlogbeat-*"),
+					resource.TestCheckResourceAttr(resourceName, "tiebreaker_field", "@timestamp"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_ESQL(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_esql("test-esql-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-esql-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "esql"),
+					resource.TestCheckResourceAttr(resourceName, "query", "FROM logs-* | WHERE event.action == \"login\" | STATS count(*) BY user.name"),
+					resource.TestCheckResourceAttr(resourceName, "language", "esql"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test ESQL security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "medium"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "60"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_MachineLearning(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_machineLearning("test-ml-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-ml-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "machine_learning"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test ML security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "critical"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "90"),
+					resource.TestCheckResourceAttr(resourceName, "anomaly_threshold", "75"),
+					resource.TestCheckResourceAttr(resourceName, "machine_learning_job_id.0", "test-ml-job"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_NewTerms(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_newTerms("test-new-terms-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-new-terms-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "new_terms"),
+					resource.TestCheckResourceAttr(resourceName, "query", "user.name:*"),
+					resource.TestCheckResourceAttr(resourceName, "language", "kuery"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test new terms security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "medium"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "50"),
+					resource.TestCheckResourceAttr(resourceName, "index.0", "logs-*"),
+					resource.TestCheckResourceAttr(resourceName, "new_terms_fields.0", "user.name"),
+					resource.TestCheckResourceAttr(resourceName, "history_window_start", "now-14d"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_SavedQuery(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_savedQuery("test-saved-query-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-saved-query-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "saved_query"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test saved query security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "low"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "30"),
+					resource.TestCheckResourceAttr(resourceName, "saved_id", "test-saved-query-id"),
+					resource.TestCheckResourceAttr(resourceName, "index.0", "logs-*"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_ThreatMatch(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_threatMatch("test-threat-match-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-threat-match-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "threat_match"),
+					resource.TestCheckResourceAttr(resourceName, "query", "destination.ip:*"),
+					resource.TestCheckResourceAttr(resourceName, "language", "kuery"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test threat match security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "high"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "80"),
+					resource.TestCheckResourceAttr(resourceName, "index.0", "logs-*"),
+					resource.TestCheckResourceAttr(resourceName, "threat_index.0", "threat-intel-*"),
+					resource.TestCheckResourceAttr(resourceName, "threat_query", "threat.indicator.type:ip"),
+					resource.TestCheckResourceAttr(resourceName, "threat_mapping.0.entries.0.field", "destination.ip"),
+					resource.TestCheckResourceAttr(resourceName, "threat_mapping.0.entries.0.type", "mapping"),
+					resource.TestCheckResourceAttr(resourceName, "threat_mapping.0.entries.0.value", "threat.indicator.ip"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceSecurityDetectionRule_Threshold(t *testing.T) {
+	resourceName := "elasticstack_kibana_security_detection_rule.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.Providers,
+		CheckDestroy:             testAccCheckSecurityDetectionRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSecurityDetectionRuleConfig_threshold("test-threshold-rule"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "test-threshold-rule"),
+					resource.TestCheckResourceAttr(resourceName, "type", "threshold"),
+					resource.TestCheckResourceAttr(resourceName, "query", "event.action:login"),
+					resource.TestCheckResourceAttr(resourceName, "language", "kuery"),
+					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test threshold security detection rule"),
+					resource.TestCheckResourceAttr(resourceName, "severity", "medium"),
+					resource.TestCheckResourceAttr(resourceName, "risk_score", "55"),
+					resource.TestCheckResourceAttr(resourceName, "index.0", "logs-*"),
+					resource.TestCheckResourceAttr(resourceName, "threshold.value", "10"),
+					resource.TestCheckResourceAttr(resourceName, "threshold.field.0", "user.name"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "rule_id"),
 				),
 			},
 		},
@@ -101,7 +305,7 @@ func testAccCheckSecurityDetectionRuleDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccSecurityDetectionRuleConfig_basic(name string) string {
+func testAccSecurityDetectionRuleConfig_query(name string) string {
 	return fmt.Sprintf(`
 provider "elasticstack" {
   kibana {}
@@ -113,9 +317,82 @@ resource "elasticstack_kibana_security_detection_rule" "test" {
   query       = "*:*"
   language    = "kuery"
   enabled     = true
-  description = "Test security detection rule"
+  description = "Test query security detection rule"
   severity    = "medium"
   risk_score  = 50
+  from        = "now-6m"
+  to          = "now"
+  interval    = "5m"
+  index       = ["logs-*"]
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_queryUpdate(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name        = "%s"
+  type        = "query"
+  query       = "*:*"
+  language    = "kuery"
+  enabled     = true
+  description = "Updated test query security detection rule"
+  severity    = "high"
+  risk_score  = 75
+  from        = "now-6m"
+  to          = "now"
+  interval    = "5m"
+  index       = ["logs-*"]
+  author      = ["Test Author"]
+  tags        = ["test", "automation"]
+  license     = "Elastic License v2"
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_eql(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name             = "%s"
+  type             = "eql"
+  query            = "process where process.name == \"cmd.exe\""
+  language         = "eql"
+  enabled          = true
+  description      = "Test EQL security detection rule"
+  severity         = "high"
+  risk_score       = 70
+  from             = "now-6m"
+  to               = "now"
+  interval         = "5m"
+  index            = ["winlogbeat-*"]
+  tiebreaker_field = "@timestamp"
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_esql(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name        = "%s"
+  type        = "esql"
+  query       = "FROM logs-* | WHERE event.action == \"login\" | STATS count(*) BY user.name"
+  language    = "esql"
+  enabled     = true
+  description = "Test ESQL security detection rule"
+  severity    = "medium"
+  risk_score  = 60
   from        = "now-6m"
   to          = "now"
   interval    = "5m"
@@ -123,7 +400,54 @@ resource "elasticstack_kibana_security_detection_rule" "test" {
 `, name)
 }
 
-func testAccSecurityDetectionRuleConfig_update(name string) string {
+func testAccSecurityDetectionRuleConfig_machineLearning(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name                     = "%s"
+  type                     = "machine_learning"
+  enabled                  = true
+  description              = "Test ML security detection rule"
+  severity                 = "critical"
+  risk_score               = 90
+  from                     = "now-6m"
+  to                       = "now"
+  interval                 = "5m"
+  anomaly_threshold        = 75
+  machine_learning_job_id  = ["test-ml-job"]
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_newTerms(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name                = "%s"
+  type                = "new_terms"
+  query               = "user.name:*"
+  language            = "kuery"
+  enabled             = true
+  description         = "Test new terms security detection rule"
+  severity            = "medium"
+  risk_score          = 50
+  from                = "now-6m"
+  to                  = "now"
+  interval            = "5m"
+  index               = ["logs-*"]
+  new_terms_fields    = ["user.name"]
+  history_window_start = "now-14d"
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_savedQuery(name string) string {
 	return fmt.Sprintf(`
 provider "elasticstack" {
   kibana {}
@@ -131,19 +455,82 @@ provider "elasticstack" {
 
 resource "elasticstack_kibana_security_detection_rule" "test" {
   name        = "%s"
-  type        = "query"
+  type        = "saved_query"
   query       = "*:*"
-  language    = "kuery"
   enabled     = true
-  description = "Updated test security detection rule"
-  severity    = "high"
-  risk_score  = 75
+  description = "Test saved query security detection rule"
+  severity    = "low"
+  risk_score  = 30
   from        = "now-6m"
   to          = "now"
   interval    = "5m"
-  author      = ["Test Author"]
-  tags        = ["test", "automation"]
-  license     = "Elastic License v2"
+  index       = ["logs-*"]
+  saved_id    = "test-saved-query-id"
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_threatMatch(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name         = "%s"
+  type         = "threat_match"
+  query        = "destination.ip:*"
+  language     = "kuery"
+  enabled      = true
+  description  = "Test threat match security detection rule"
+  severity     = "high"
+  risk_score   = 80
+  from         = "now-6m"
+  to           = "now"
+  interval     = "5m"
+  index        = ["logs-*"]
+  threat_index = ["threat-intel-*"]
+  threat_query = "threat.indicator.type:ip"
+  
+  threat_mapping = [
+    {
+      entries = [
+        {
+          field = "destination.ip"
+          type  = "mapping"
+          value = "threat.indicator.ip"
+        }
+      ]
+    }
+  ]
+}
+`, name)
+}
+
+func testAccSecurityDetectionRuleConfig_threshold(name string) string {
+	return fmt.Sprintf(`
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_security_detection_rule" "test" {
+  name        = "%s"
+  type        = "threshold"
+  query       = "event.action:login"
+  language    = "kuery"
+  enabled     = true
+  description = "Test threshold security detection rule"
+  severity    = "medium"
+  risk_score  = 55
+  from        = "now-6m"
+  to          = "now"
+  interval    = "5m"
+  index       = ["logs-*"]
+  
+  threshold = {
+    value = 10
+    field = ["user.name"]
+  }
 }
 `, name)
 }
