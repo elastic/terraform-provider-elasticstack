d2aa4d2c90cd9313c225fc958abf4260c83da587
Toby Brain <toby.brain@elastic.co>
2025-09-19 09:23:30 +1000

    Fix tests

--- internal/fleet/output/resource_test.go
+++ internal/fleet/output/resource_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
