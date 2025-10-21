5ca1f3c017437b44bbde72cddb54676310871e04
Toby Brain <toby.brain@elastic.co>
2025-10-21 16:34:54 +1100

    Optional:true

--- internal/elasticsearch/ml/anomaly_detection_job/schema.go
+++ internal/elasticsearch/ml/anomaly_detection_job/schema.go
@@ -72,6 +72,7 @@ func GetSchema() schema.Schema {
 						MarkdownDescription: "The size of the interval that the analysis is aggregated into, typically between 15m and 1h. If the anomaly detector is expecting to see data at near real-time frequency, then the bucket_span should be set to a value around 10 times the time between ingested documents. For example, if data comes every second, bucket_span should be 10s; if data comes every 5 minutes, bucket_span should be 50m. For sparse or batch data, use larger bucket_span values.",
 						Default:             stringdefault.StaticString("5m"),
 						Computed:            true,
+						Optional:            true,
 						Validators: []validator.String{
 							stringvalidator.RegexMatches(regexp.MustCompile(`^\d+[nsumdh]$`), "must be a valid time interval (e.g., 15m, 1h)"),
 						},
