93725f914c79699eb46a6043377bacf12d390464
Toby Brain <toby.brain@elastic.co>
2025-11-14 08:12:09 +1100

    Extract integration policy test config

---
+++ internal/fleet/integration_policy/testdata/TestAccResourceIntegrationPolicy/update/integration_policy.tf
@@ -0,0 +1,68 @@
+variable "policy_name" {
+  description = "The integration policy name"
+  type        = string
+}
+
+variable "integration_name" {
+  description = "The integration name"
+  type        = string
+  default     = "tcp"
+}
+
+variable "integration_version" {
+  description = "The integration version"
+  type        = string
+  default     = "1.16.0"
+}
+
+provider "elasticstack" {
+  elasticsearch {}
+  kibana {}
+}
+
+resource "elasticstack_fleet_integration" "test_policy" {
+  name    = var.integration_name
+  version = var.integration_version
+  force   = true
+}
+
+resource "elasticstack_fleet_agent_policy" "test_policy" {
+  name            = "${var.policy_name} Agent Policy"
+  namespace       = "default"
+  description     = "IntegrationPolicyTest Agent Policy"
+  monitor_logs    = true
+  monitor_metrics = true
+  skip_destroy    = false
+}
+
+data "elasticstack_fleet_enrollment_tokens" "test_policy" {
+  policy_id = elasticstack_fleet_agent_policy.test_policy.policy_id
+}
+
+resource "elasticstack_fleet_integration_policy" "test_policy" {
+  name                = var.policy_name
+  namespace           = "default"
+  description         = "Updated Integration Policy"
+  agent_policy_id     = elasticstack_fleet_agent_policy.test_policy.policy_id
+  integration_name    = elasticstack_fleet_integration.test_policy.name
+  integration_version = elasticstack_fleet_integration.test_policy.version
+
+  input {
+    input_id = "tcp-tcp"
+    enabled  = false
+    streams_json = jsonencode({
+      "tcp.generic" : {
+        "enabled" : false
+        "vars" : {
+          "listen_address" : "localhost"
+          "listen_port" : 8085
+          "data_stream.dataset" : "tcp.generic"
+          "tags" : []
+          "syslog_options" : "field: message"
+          "ssl" : ""
+          "custom" : ""
+        }
+      }
+    })
+  }
+}
---
+++ internal/fleet/integration_policy/testdata/TestAccResourceIntegrationPolicySecrets/single_valued_secrets/import_test/integration_policy.tf
@@ -0,0 +1,116 @@
+variable "policy_name" {
+  description = "The integration policy name"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key for access"
+  type        = string
+}
+
+variable "integration_name" {
+  description = "The integration name"
+  type        = string
+  default     = "aws_logs"
+}
+
+variable "integration_version" {
+  description = "The integration version"
+  type        = string
+  default     = "1.4.0"
+}
+
+variable "default_region" {
+  description = "AWS default region"
+  type        = string
+  default     = "us-east-2"
+}
+
+provider "elasticstack" {
+  elasticsearch {}
+  kibana {}
+}
+
+resource "elasticstack_fleet_integration" "test_policy" {
+  name    = var.integration_name
+  version = var.integration_version
+  force   = true
+}
+
+resource "elasticstack_fleet_agent_policy" "test_policy" {
+  name            = "${var.policy_name} Agent Policy"
+  namespace       = "default"
+  description     = "IntegrationPolicyTest Agent Policy"
+  monitor_logs    = true
+  monitor_metrics = true
+  skip_destroy    = false
+}
+
+data "elasticstack_fleet_enrollment_tokens" "test_policy" {
+  policy_id = elasticstack_fleet_agent_policy.test_policy.policy_id
+}
+
+resource "elasticstack_fleet_integration_policy" "test_policy" {
+  name                = var.policy_name
+  namespace           = "default"
+  description         = "Updated Integration Policy"
+  agent_policy_id     = elasticstack_fleet_agent_policy.test_policy.policy_id
+  integration_name    = elasticstack_fleet_integration.test_policy.name
+  integration_version = elasticstack_fleet_integration.test_policy.version
+
+  vars_json = jsonencode({
+    "access_key_id" : "placeholder"
+    "secret_access_key" : "${var.secret_key} ${var.policy_name}"
+    "session_token" : "placeholder"
+    "endpoint" : "endpoint"
+    "default_region" : var.default_region
+  })
+
+  input {
+    input_id = "aws_logs-aws-cloudwatch"
+    enabled  = false
+    streams_json = jsonencode({
+      "aws_logs.generic" = {
+        enabled = false
+        vars = {
+          "number_of_workers" : 1,
+          "log_streams" : [],
+          "start_position" : "beginning",
+          "scan_frequency" : "2m",
+          "api_timeput" : "120s",
+          "api_sleep" : "200ms",
+          "tags" : ["forwarded"],
+          "preserve_original_event" : false,
+          "data_stream.dataset" : "aws_logs.generic",
+          "custom" : "",
+        }
+      }
+    })
+  }
+
+  input {
+    input_id = "aws_logs-aws-s3"
+    enabled  = false
+    streams_json = jsonencode({
+      "aws_logs.generic" = {
+        enabled = false
+        vars = {
+          "number_of_workers" : 1,
+          "bucket_list_interval" : "120s",
+          "file_selectors" : "",
+          "fips_enabled" : false,
+          "include_s3_metadata" : [],
+          "max_bytes" : "20MiB",
+          "max_number_of_messages" : 5,
+          "parsers" : "",
+          "sqs.max_receive_count" : 5,
+          "sqs.wait_time" : "20s",
+          "tags" : ["forwarded"],
+          "preserve_original_event" : false,
+          "data_stream.dataset" : "aws_logs.generic",
+          "custom" : "",
+        }
+      }
+    })
+  }
+}
