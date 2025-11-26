cb6e1714d63b503eb9f94d56858f4573cf3a6761
Nick Benoit <nick.benoit@elastic.co>
2025-11-26 13:16:05 -0700

    Add security list

--- provider/plugin_framework.go
+++ provider/plugin_framework.go
@@ -32,6 +32,7 @@ import (
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/import_saved_objects"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/maintenance_window"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/security_detection_rule"
+	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/security_list"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/security_list_item"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/spaces"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/synthetics/monitor"
@@ -148,6 +149,7 @@ func (p *Provider) resources(ctx context.Context) []func() resource.Resource {
 func (p *Provider) experimentalResources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		security_list_item.NewResource,
+		security_list.NewResource,
 	}
 }
 
