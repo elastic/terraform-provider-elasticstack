3980a354d2b39e74a73ee937002a364318ee4a53
Nick Benoit <nick.benoit@elastic.co>
2025-11-27 15:23:32 -0700

    Add plugin to provider

--- provider/plugin_framework.go
+++ provider/plugin_framework.go
@@ -33,6 +33,7 @@ import (
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/import_saved_objects"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/maintenance_window"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/security_detection_rule"
+	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/security_exception_list"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/security_list"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/spaces"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/synthetics/monitor"
@@ -150,6 +151,7 @@ func (p *Provider) resources(ctx context.Context) []func() resource.Resource {
 func (p *Provider) experimentalResources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		security_list.NewResource,
+		security_exception_list.NewResource,
 	}
 }
 
