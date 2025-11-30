f020f0d377bff18e70ff17c205092971da4a9fe3
Nick Benoit <nick.benoit@elastic.co>
2025-11-30 12:17:51 -0700

    Simplify examples

--- examples/resources/elasticstack_kibana_security_exception_list/resource.tf
+++ examples/resources/elasticstack_kibana_security_exception_list/resource.tf
@@ -1,9 +1,10 @@
-resource "elasticstack_kibana_security_exception_list" "example" {
-  list_id        = "my-detection-exception-list"
-  name           = "My Detection Exception List"
-  description    = "List of exceptions for security detection rules"
-  type           = "detection"
-  namespace_type = "single"
+resource "elasticstack_kibana_security_exception_list" "endpoint" {
+  list_id        = "my-endpoint-exception-list"
+  name           = "My Endpoint Exception List"
+  description    = "List of endpoint exceptions"
+  type           = "endpoint"
+  namespace_type = "agnostic"
 
-  tags = ["security", "detections"]
+  os_types = ["linux", "windows", "macos"]
+  tags     = ["endpoint", "security"]
 }
