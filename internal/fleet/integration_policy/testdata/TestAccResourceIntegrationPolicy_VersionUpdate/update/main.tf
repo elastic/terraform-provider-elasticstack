5f25ea674555c9be8d35d521b6b7d430858c4466
James Garside <james.garside@elastic.co>
2026-01-14 09:50:07 +0000

    Address Tobio's review: Move test config to external files + add test plan
    Changes:
    - Move TestAccResourceIntegrationPolicy_VersionUpdate config to external .tf files
      per reviewer request to follow established pattern
    - Create testdata/TestAccResourceIntegrationPolicy_VersionUpdate/create/main.tf
    - Create testdata/TestAccResourceIntegrationPolicy_VersionUpdate/update/main.tf
    - Update test to use ConfigDirectory instead of inline Config
    - Add comprehensive edge case test plan documentation
    
    This addresses the final review comment about externalizing test configuration.
    All other review comments were addressed in previous commit (02fa5fe8).
    
    Related: https://github.com/elastic/terraform-provider-elasticstack/pull/1616

---
+++ internal/fleet/integration_policy/testdata/TestAccResourceIntegrationPolicy_VersionUpdate/update/main.tf
@@ -0,0 +1,22 @@
+variable "policy_name" {
+  description = "The integration policy name"
+  type        = string
+}
+
+provider "elasticstack" {
+  elasticsearch {}
+  kibana {}
+}
+
+resource "elasticstack_fleet_agent_policy" "test_policy" {
+  name      = var.policy_name
+  namespace = "default"
+}
+
+resource "elasticstack_fleet_integration_policy" "test_policy" {
+  name                = "${var.policy_name}-integration"
+  namespace           = "default"
+  agent_policy_id     = elasticstack_fleet_agent_policy.test_policy.policy_id
+  integration_name    = "tcp"
+  integration_version = "1.17.0"  # Updated version
+}
