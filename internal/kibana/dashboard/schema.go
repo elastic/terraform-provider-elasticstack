9bf85c914c350b6575358434a294c6b8f22acea1
Toby Brain <toby.brain@elastic.co>
2026-01-14 22:19:54 +1100

    Fix build

--- internal/kibana/dashboard/schema.go
+++ internal/kibana/dashboard/schema.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
