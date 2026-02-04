c56f992218519f3701143423dbebd64be966ee82
Nick Clark <innernetz@gmail.com>
2026-02-05 09:12:48 +1100

    Update internal/kibana/alerting_rule/schema.go
    Co-authored-by: Toby Brain <toby.brain@elastic.co>

--- internal/kibana/alerting_rule/schema.go
+++ internal/kibana/alerting_rule/schema.go
@@ -87,7 +87,7 @@ func getSchema() schema.Schema {
 				CustomType:  jsontypes.NormalizedType{},
 			},
 			"rule_type_id": schema.StringAttribute{
-				Description: "The ID of the rule type that you want to call when the rule is scheduled to run. For more information about the valid values, list the rule types using Get rule types API or refer to the Rule types documentation.",
+				Description: "The ID of the rule type that you want to call when the rule is scheduled to run. For more information about the valid values, list the rule types using [Get rule types API](https://www.elastic.co/guide/en/kibana/master/list-rule-types-api.html) or refer to the [Rule types documentation](https://www.elastic.co/guide/en/kibana/master/rule-types.html).",
 				Required:    true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
