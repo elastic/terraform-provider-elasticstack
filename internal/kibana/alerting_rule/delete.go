a15fc51b48289b8fe2fbf9141b690c29270b090c
Nick Clark <nicholas.clark@elastic.co>
2026-02-06 11:05:41 +1100

    wip

--- internal/kibana/alerting_rule/delete.go
+++ internal/kibana/alerting_rule/delete.go
@@ -3,8 +3,7 @@ package alerting_rule
 import (
 	"context"
 
-	"github.com/elastic/terraform-provider-elasticstack/internal/clients/kibana"
-	"github.com/elastic/terraform-provider-elasticstack/internal/diagutil"
+	"github.com/elastic/terraform-provider-elasticstack/internal/clients/kibana_oapi"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 )
 
@@ -24,9 +23,16 @@ func (r *Resource) Delete(ctx context.Context, req resource.DeleteRequest, resp 
 
 	ruleID, spaceID := state.getRuleIDAndSpaceID()
 
-	deleteDiags := kibana.DeleteAlertingRule(ctx, r.client, ruleID, spaceID)
-	if deleteDiags.HasError() {
-		resp.Diagnostics.Append(diagutil.FrameworkDiagsFromSDK(deleteDiags)...)
+	// Get kbapi client
+	oapiClient, err := r.client.GetKibanaOapiClient()
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to get Kibana client", err.Error())
+		return
+	}
+
+	deleteDiags := kibana_oapi.DeleteAlertingRule(ctx, oapiClient, spaceID, ruleID)
+	resp.Diagnostics.Append(deleteDiags...)
+	if resp.Diagnostics.HasError() {
 		return
 	}
 }
