fd7cde0d7b9f2c4a3313e977dd7886bf58790042
Toby Brain <toby.brain@elastic.co>
2026-02-26 09:50:29 +1100

    Don't set os_types to null if they were known in the plan.  (#1740)
    * Add acceptance test reproduction
    
    * Leave ostypes unchanged if it's known in plan, but not returned by the API
    
    * Tidy up acceptance tests
    
    * Changelog
    
    * Fix tests

---
+++ internal/kibana/securityexceptionlist/testdata/TestAccResourceExceptionList/with_optional/main.tf
@@ -0,0 +1,39 @@
+variable "list_id" {
+  description = "The exception list ID"
+  type        = string
+}
+
+variable "name" {
+  description = "The exception list name"
+  type        = string
+}
+
+variable "description" {
+  description = "The exception list description"
+  type        = string
+}
+
+variable "type" {
+  description = "The exception list type"
+  type        = string
+}
+
+variable "namespace_type" {
+  description = "The namespace type"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the exception list"
+  type        = list(string)
+}
+
+resource "elasticstack_kibana_security_exception_list" "test" {
+  list_id        = var.list_id
+  name           = var.name
+  description    = var.description
+  type           = var.type
+  namespace_type = var.namespace_type
+  os_types       = ["linux", "windows"]
+  tags           = var.tags
+}
