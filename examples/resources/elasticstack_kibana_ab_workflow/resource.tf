b2e8f0c59597acd427d7c251869d870893aaa0fb
Alec Carpenter <ajcarp29@gmail.com>
2026-03-11 14:57:11 -0400

    add examples

---
+++ examples/resources/elasticstack_kibana_ab_workflow/resource.tf
@@ -0,0 +1,22 @@
+provider "elasticstack" {
+  kibana {}
+}
+
+resource "elasticstack_kibana_ab_workflow" "my_workflow" {
+  configuration = <<-EOT
+name: My Workflow
+description: An example workflow
+enabled: true
+triggers:
+  - type: manual
+inputs:
+  - name: message
+    type: string
+    default: "hello world"
+steps:
+  - name: hello_world_step
+    type: console
+    with:
+      message: "{{ inputs.message }}"
+EOT
+}
