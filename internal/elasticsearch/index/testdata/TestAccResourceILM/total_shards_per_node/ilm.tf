8dee91c4f001fd22e3c689985ee1a220ac8b1f67
Toby Brain <toby.brain@elastic.co>
2026-03-17 09:19:28 +1100

    Migrate ILM acceptance tests to ConfigDirectory (#1896)
    * Migrate ILM acceptance tests
    
    * Changelog

---
+++ internal/elasticsearch/index/testdata/TestAccResourceILM/total_shards_per_node/ilm.tf
@@ -0,0 +1,41 @@
+provider "elasticstack" {
+  elasticsearch {}
+}
+
+variable "policy_name" {
+  type = string
+}
+
+resource "elasticstack_elasticsearch_index_lifecycle" "test" {
+  name = var.policy_name
+
+  hot {
+    min_age = "1h"
+
+    set_priority {
+      priority = 0
+    }
+
+    rollover {
+      max_age = "2d"
+    }
+  }
+
+  warm {
+    min_age = "0ms"
+
+    set_priority {
+      priority = 60
+    }
+
+    readonly {}
+
+    allocate {
+      exclude = jsonencode({
+        box_type = "hot"
+      })
+      number_of_replicas    = 1
+      total_shards_per_node = 200
+    }
+  }
+}
