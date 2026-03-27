4cadd007564eaa2095d4a5d48036410ba58cddb8
Alec Carpenter <ajcarp29@gmail.com>
2026-03-27 15:10:31 -0400

    fmt

--- internal/kibana/exportagentbuilder/tool/testdata/TestAccDataSourceKibanaExportABTool/read/tool.tf
+++ internal/kibana/exportagentbuilder/tool/testdata/TestAccDataSourceKibanaExportABTool/read/tool.tf
@@ -8,10 +8,10 @@ provider "elasticstack" {
 }
 
 resource "elasticstack_kibana_agentbuilder_tool" "test" {
-  tool_id = var.tool_id
-  type    = "esql"
+  tool_id     = var.tool_id
+  type        = "esql"
   description = "Test ESQL tool"
-  tags    = ["test"]
+  tags        = ["test"]
   configuration = jsonencode({
     query  = "FROM logs | LIMIT 10"
     params = {}
