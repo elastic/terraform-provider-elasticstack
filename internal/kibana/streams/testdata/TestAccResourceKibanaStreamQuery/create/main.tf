2b5fe29aa81a1056f074328b74320ade81208916
Coen Warmer <coen.warmer@gmail.com>
2026-03-31 13:40:29 +0200

    Update test again

--- internal/kibana/streams/testdata/TestAccResourceKibanaStreamQuery/create/main.tf
+++ internal/kibana/streams/testdata/TestAccResourceKibanaStreamQuery/create/main.tf
@@ -8,12 +8,23 @@ provider "elasticstack" {
   kibana {}
 }
 
+# Create the parent wired stream first so that its $.{name} data view exists
+# and can be referenced in the query stream's FROM clause.
+resource "elasticstack_kibana_stream" "parent" {
+  name     = "logs.otel.testacc-w${var.suffix}"
+  space_id = "default"
+
+  wired_config = {}
+}
+
 resource "elasticstack_kibana_stream" "query" {
-  name        = "logs.otel.testacc-q${var.suffix}"
+  name        = "logs.otel.testacc-w${var.suffix}.view"
   space_id    = "default"
   description = "Test query stream"
 
   query_config = {
-    esql = "FROM $.logs.otel | LIMIT 10"
+    esql = "FROM $.logs.otel.testacc-w${var.suffix} | LIMIT 10"
   }
+
+  depends_on = [elasticstack_kibana_stream.parent]
 }
--- internal/kibana/streams/testdata/TestAccResourceKibanaStreamQuery/update/main.tf
+++ internal/kibana/streams/testdata/TestAccResourceKibanaStreamQuery/update/main.tf
@@ -8,12 +8,21 @@ provider "elasticstack" {
   kibana {}
 }
 
+resource "elasticstack_kibana_stream" "parent" {
+  name     = "logs.otel.testacc-w${var.suffix}"
+  space_id = "default"
+
+  wired_config = {}
+}
+
 resource "elasticstack_kibana_stream" "query" {
-  name        = "logs.otel.testacc-q${var.suffix}"
+  name        = "logs.otel.testacc-w${var.suffix}.view"
   space_id    = "default"
   description = "Updated query stream"
 
   query_config = {
-    esql = "FROM $.logs.otel | WHERE @timestamp > NOW() - 1 HOUR | LIMIT 10"
+    esql = "FROM $.logs.otel.testacc-w${var.suffix} | WHERE @timestamp > NOW() - 1 HOUR | LIMIT 10"
   }
+
+  depends_on = [elasticstack_kibana_stream.parent]
 }
