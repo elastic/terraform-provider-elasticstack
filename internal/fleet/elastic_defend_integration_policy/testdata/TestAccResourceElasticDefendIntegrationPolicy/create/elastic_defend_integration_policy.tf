f5e23b0fe56b61386378c4c6fc801df141f56db9
Toby Brain <toby.brain@elastic.co>
2026-04-07 07:37:51 +1000

    Broaden acceptance test coverage

--- internal/fleet/elastic_defend_integration_policy/testdata/TestAccResourceElasticDefendIntegrationPolicy/create/elastic_defend_integration_policy.tf
+++ internal/fleet/elastic_defend_integration_policy/testdata/TestAccResourceElasticDefendIntegrationPolicy/create/elastic_defend_integration_policy.tf
@@ -17,6 +17,7 @@ resource "elasticstack_fleet_elastic_defend_integration_policy" "test" {
   name                = var.policy_name
   namespace           = "default"
   agent_policy_id     = elasticstack_fleet_agent_policy.test.policy_id
+  enabled             = true
   integration_version = "8.14.0"
   preset              = "NGAv1"
 
@@ -26,20 +27,68 @@ resource "elasticstack_fleet_elastic_defend_integration_policy" "test" {
         process = true
         network = true
         file    = true
+        dns     = true
       }
       malware = {
+        mode          = "prevent"
+        blocklist     = true
+        notify_user   = true
+        on_write_scan = true
+      }
+      ransomware = {
         mode = "prevent"
+      }
+      memory_protection = {
+        mode = "detect"
+      }
+      behavior_protection = {
+        mode               = "prevent"
+        reputation_service = true
+      }
+      logging = {
+        file = "info"
       }
     }
     mac = {
       events = {
         process = true
+        file    = true
+      }
+      malware = {
+        mode = "prevent"
+      }
+      memory_protection = {
+        mode = "prevent"
+      }
+      behavior_protection = {
+        mode               = "detect"
+        reputation_service = true
+      }
+      logging = {
+        file = "warning"
       }
     }
     linux = {
       events = {
-        process = true
-        network = true
+        process      = true
+        network      = true
+        file         = true
+        session_data = true
+        tty_io       = false
+      }
+      malware = {
+        mode      = "detect"
+        blocklist = true
+      }
+      memory_protection = {
+        mode = "prevent"
+      }
+      behavior_protection = {
+        mode               = "detect"
+        reputation_service = true
+      }
+      logging = {
+        file = "warning"
       }
     }
   }
