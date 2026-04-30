535dab41497d584a6fe83d19b94e97e1de0cb160
Toby Brain <toby.brain@elastic.co>
2026-04-30 12:17:37 +1000

    Task 2.4: Convert Delete method to package-level deleteUser function

--- internal/elasticsearch/security/user/delete.go
+++ internal/elasticsearch/security/user/delete.go
@@ -22,27 +22,9 @@ import (
 
 	"github.com/elastic/terraform-provider-elasticstack/internal/clients"
 	"github.com/elastic/terraform-provider-elasticstack/internal/clients/elasticsearch"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 )
 
-func (r *userResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data Data
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	compID, diags := clients.CompositeIDFromStrFw(data.ID.ValueString())
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	client, diags := r.Client().GetElasticsearchClient(ctx, data.ElasticsearchConnection)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	resp.Diagnostics.Append(elasticsearch.DeleteUser(ctx, client, compID.ResourceID)...)
+func deleteUser(ctx context.Context, client *clients.ElasticsearchScopedClient, resourceID string, _ Data) diag.Diagnostics {
+	return elasticsearch.DeleteUser(ctx, client, resourceID)
 }
