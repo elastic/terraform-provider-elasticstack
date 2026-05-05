783c19149f85a8d971a6c2f35a35f7dfd0ae3ca9
Toby Brain <tobias@modestmind.net>
2026-05-06 09:33:15 +1000

    openspec: fix ILM delete when policy is referenced by indices (#1999)
    Create change proposal and repro acceptance test for the cross-dependency
    failure described in #1999. When Fleet-managed data streams create backing
    indices that reference an ILM policy, destroying the ILM resource fails because
    Elasticsearch rejects deletion of an in-use policy.
    
    This commit adds:
    - OpenSpec change proposal with design, delta spec, and task list
    - Acceptance test TestAccResourceIntegration_destroyWithILMCrossDependency that
      reproduces the failure: Fleet integration + ILM attachment + data stream,

---
+++ internal/fleet/integration/testdata/TestAccResourceIntegration_destroyWithILMCrossDependency/create/main.tf
@@ -0,0 +1,27 @@
+provider "elasticstack" {
+  elasticsearch {}
+  kibana {}
+}
+
+resource "elasticstack_fleet_integration" "test_integration" {
+  name    = "system"
+  version = "1.18.0"
+  force   = true
+}
+
+resource "elasticstack_elasticsearch_index_lifecycle" "test" {
+  name = "test-fleet-ilm-policy"
+
+  hot {
+    rollover {
+      max_age = "1d"
+    }
+  }
+}
+
+resource "elasticstack_elasticsearch_index_template_ilm_attachment" "test" {
+  depends_on = [elasticstack_fleet_integration.test_integration]
+
+  index_template = "logs-system.syslog"
+  lifecycle_name = elasticstack_elasticsearch_index_lifecycle.test.name
+}
