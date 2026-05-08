95d7ce88a697753e44500b0fef2ba134fd3400ac
Toby Brain <toby.brain@elastic.co>
2026-05-08 18:31:25 +1000

    Fix remaining fs block syntax in ILM test fixtures
    Apply the same 'fs = {' -> 'fs {' fix to the FrozenPhase and
    SearchableSnapshotPhases test fixtures, clearing all remaining
    instances of the attribute-syntax error in the ilm testdata.

--- internal/elasticsearch/index/ilm/testdata/TestAccResourceILMFrozenPhase/create/ilm.tf
+++ internal/elasticsearch/index/ilm/testdata/TestAccResourceILMFrozenPhase/create/ilm.tf
@@ -13,7 +13,7 @@ variable "repository_name" {
 resource "elasticstack_elasticsearch_snapshot_repository" "repo" {
   name = var.repository_name
 
-  fs = {
+  fs {
     location                  = "/tmp/snapshots"
     compress                  = true
     max_restore_bytes_per_sec = "20mb"
