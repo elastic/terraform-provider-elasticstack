afb3e60d1d011523a36071fa5affc5315883442c
Ed Savage <ed.savage@elastic.co>
2026-05-13 11:41:19 +1200

    Formatting

--- internal/elasticsearch/index/indices/testdata/TestAccIndicesDataSource_Target_DefaultAndExplicitAll/no_target/main.tf
+++ internal/elasticsearch/index/indices/testdata/TestAccIndicesDataSource_Target_DefaultAndExplicitAll/no_target/main.tf
@@ -7,10 +7,10 @@ provider "elasticstack" {
 }
 
 resource "elasticstack_elasticsearch_index" "fixture" {
-  name                 = var.fixture_index
-  number_of_shards     = 1
-  number_of_replicas   = 0
-  deletion_protection  = false
+  name                = var.fixture_index
+  number_of_shards    = 1
+  number_of_replicas  = 0
+  deletion_protection = false
 }
 
 data "elasticstack_elasticsearch_indices" "all_default" {
