aa3cf6c3dcff621904acc85be4ab0c90bf310918
Toby Brain <toby.brain@elastic.co>
2026-05-17 08:30:16 +1000

    nolint

--- internal/clients/config/debug.go
+++ internal/clients/config/debug.go
@@ -34,6 +34,7 @@ const logRespMsg = `%s API Response for [%s] Details:
 %s
 -----------------------------------------------------`
 
+//nolint:staticcheck // Temporary lint suppression for interface compliance assertion.
 var _ elastictransport.Logger = &debugLogger{}
 
 type debugLogger struct {
