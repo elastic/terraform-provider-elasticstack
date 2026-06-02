0cf17ec6bf8eb7e161903d0952a0087904879e55
Toby Brain <toby.brain@elastic.co>
2026-06-02 12:49:51 +1000

    Move entity acceptance tests to parent package to fix CI 'Invalid resource type' error
    The CI acceptance test runner was reporting 'Invalid resource type' when running
    tests from the entity sub-package. Moving acceptance tests to the existing
    security_entity_store_test package (as external tests in acc_test.go) ensures
    the test binary is built correctly with all provider registrations.

--- internal/kibana/security_entity_store/acc_test.go
+++ internal/kibana/security_entity_store/acc_test.go
@@ -216,6 +216,118 @@ func TestAccDataSourceKibanaSecurityEntityStoreStatus_basic(t *testing.T) {
 	})
 }
 
+func TestAccResourceKibanaSecurityEntityStoreEntity_host(t *testing.T) {
+	skipIfUnsupportedEntity(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("create_host"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("elasticstack_kibana_security_entity_store_entity.test", "id"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_security_entity_store_entity.test", "entity_type", "host"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_security_entity_store_entity.test", "entity_id", "host:acc-test-host-01"),
+					resource.TestCheckResourceAttrSet("elasticstack_kibana_security_entity_store_entity.test", "document_json"),
+				),
+			},
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("create_host"),
+				PlanOnly:                 true,
+			},
+		},
+	})
+}
+
+func TestAccResourceKibanaSecurityEntityStoreEntity_updateHost(t *testing.T) {
+	skipIfUnsupportedEntity(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("create_host"),
+			},
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("update_host"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("elasticstack_kibana_security_entity_store_entity.test", "id"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_security_entity_store_entity.test", "entity_type", "host"),
+					resource.TestCheckResourceAttr("elasticstack_kibana_security_entity_store_entity.test", "entity_id", "host:acc-test-host-01"),
+				),
+			},
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("update_host"),
+				PlanOnly:                 true,
+			},
+		},
+	})
+}
+
+func TestAccResourceKibanaSecurityEntityStoreEntity_import(t *testing.T) {
+	skipIfUnsupportedEntity(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("create_host"),
+			},
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("create_host"),
+				ResourceName:             "elasticstack_kibana_security_entity_store_entity.test",
+				ImportState:              true,
+				ImportStateVerify:        true,
+				ImportStateVerifyIgnore:  []string{"force"},
+			},
+		},
+	})
+}
+
+func TestAccResourceKibanaSecurityEntityStoreEntity_jsonFallback(t *testing.T) {
+	skipIfUnsupportedEntity(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("entity_json"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("elasticstack_kibana_security_entity_store_entity.test", "id"),
+					resource.TestCheckResourceAttrSet("elasticstack_kibana_security_entity_store_entity.test", "document_json"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceKibanaSecurityEntityStoreEntity_entityJsonConflict(t *testing.T) {
+	skipIfUnsupportedEntity(t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				ProtoV6ProviderFactories: acctest.Providers,
+				ConfigDirectory:          acctest.NamedTestCaseDirectory("entity_json_conflict"),
+				ExpectError:              regexp.MustCompile("conflict|ConflictsWith"),
+			},
+		},
+	})
+}
+
+func skipIfUnsupportedEntity(t *testing.T) {
+	versionutils.SkipIfUnsupported(t, securityentitystore.MinVersion, versionutils.FlavorAny)
+}
+
 func skipIfUnsupported(t *testing.T) {
 	versionutils.SkipIfUnsupported(t, securityentitystore.MinVersion, versionutils.FlavorAny)
 }
