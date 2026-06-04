714b3a28ce908353441f9452ed60136e9536fde2
Toby Brain <toby.brain@elastic.co>
2026-06-04 13:28:51 +1000

    fix(lint): fix import ordering in dashboard resource

--- internal/kibana/dashboard/resource.go
+++ internal/kibana/dashboard/resource.go
@@ -22,9 +22,9 @@ import (
 
 	"github.com/elastic/terraform-provider-elasticstack/internal/entitycore"
 	"github.com/elastic/terraform-provider-elasticstack/internal/kibana/dashboard/models"
-	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
 
 var (
